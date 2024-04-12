1c3baddbb005e5592bfb981f17b55de4d97c93df
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2024-04-12 13:51:58 +0200

    Transform optimize function into an iterator
    This means we can see the temporary results while the sa algorithm works its magic

--- src/ilp/solvers.rs
+++ src/ilp/solvers.rs
@@ -4,14 +4,14 @@ use super::{Config, FeasableConfig};
 
 use std::collections::BTreeSet;
 
-pub trait FeasabilitySolver {
+pub trait FeasabilitySolver<'a> {
     fn restore_feasability_exclude(
         &self,
         config: &Config,
         exclude_list: &BTreeSet<&FeasableConfig>,
-    ) -> Option<FeasableConfig>;
+    ) -> Option<FeasableConfig<'a>>;
 
-    fn restore_feasability(&self, config: &Config) -> Option<FeasableConfig> {
+    fn restore_feasability(&self, config: &Config) -> Option<FeasableConfig<'a>> {
         let exclude_list = BTreeSet::new();
         self.restore_feasability_exclude(config, &exclude_list)
     }
