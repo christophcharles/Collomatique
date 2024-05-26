55d7cf4fe70c395db0d8dca042d0301674b01924
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2024-05-26 15:35:58 +0200

    Add precomputation and try to avoid recomputation of every constraints when configuration only change by a few variables

--- src/ilp/mat_repr/sparse/tests.rs
+++ src/ilp/mat_repr/sparse/tests.rs
@@ -91,22 +91,102 @@ fn test_is_feasable() {
     let sprs_problem = &pb.pb_repr;
 
     use crate::ilp::mat_repr::ConfigRepr;
-    assert_eq!(config_0.cfg_repr.is_feasable(sprs_problem), false);
-    assert_eq!(config_1.cfg_repr.is_feasable(sprs_problem), true);
-    assert_eq!(config_2.cfg_repr.is_feasable(sprs_problem), false);
-    assert_eq!(config_3.cfg_repr.is_feasable(sprs_problem), false);
-    assert_eq!(config_4.cfg_repr.is_feasable(sprs_problem), false);
-    assert_eq!(config_5.cfg_repr.is_feasable(sprs_problem), true);
-    assert_eq!(config_6.cfg_repr.is_feasable(sprs_problem), false);
-    assert_eq!(config_7.cfg_repr.is_feasable(sprs_problem), false);
-    assert_eq!(config_8.cfg_repr.is_feasable(sprs_problem), true);
-    assert_eq!(config_9.cfg_repr.is_feasable(sprs_problem), false);
-    assert_eq!(config_a.cfg_repr.is_feasable(sprs_problem), true);
-    assert_eq!(config_b.cfg_repr.is_feasable(sprs_problem), false);
-    assert_eq!(config_c.cfg_repr.is_feasable(sprs_problem), false);
-    assert_eq!(config_d.cfg_repr.is_feasable(sprs_problem), false);
-    assert_eq!(config_e.cfg_repr.is_feasable(sprs_problem), false);
-    assert_eq!(config_f.cfg_repr.is_feasable(sprs_problem), false);
+    assert_eq!(
+        config_0
+            .cfg_repr
+            .is_feasable(sprs_problem, &config_0.cfg_repr.precompute(sprs_problem)),
+        false
+    );
+    assert_eq!(
+        config_1
+            .cfg_repr
+            .is_feasable(sprs_problem, &config_1.cfg_repr.precompute(sprs_problem)),
+        true
+    );
+    assert_eq!(
+        config_2
+            .cfg_repr
+            .is_feasable(sprs_problem, &config_2.cfg_repr.precompute(sprs_problem)),
+        false
+    );
+    assert_eq!(
+        config_3
+            .cfg_repr
+            .is_feasable(sprs_problem, &config_3.cfg_repr.precompute(sprs_problem)),
+        false
+    );
+    assert_eq!(
+        config_4
+            .cfg_repr
+            .is_feasable(sprs_problem, &config_4.cfg_repr.precompute(sprs_problem)),
+        false
+    );
+    assert_eq!(
+        config_5
+            .cfg_repr
+            .is_feasable(sprs_problem, &config_5.cfg_repr.precompute(sprs_problem)),
+        true
+    );
+    assert_eq!(
+        config_6
+            .cfg_repr
+            .is_feasable(sprs_problem, &config_6.cfg_repr.precompute(sprs_problem)),
+        false
+    );
+    assert_eq!(
+        config_7
+            .cfg_repr
+            .is_feasable(sprs_problem, &config_7.cfg_repr.precompute(sprs_problem)),
+        false
+    );
+    assert_eq!(
+        config_8
+            .cfg_repr
+            .is_feasable(sprs_problem, &config_8.cfg_repr.precompute(sprs_problem)),
+        true
+    );
+    assert_eq!(
+        config_9
+            .cfg_repr
+            .is_feasable(sprs_problem, &config_9.cfg_repr.precompute(sprs_problem)),
+        false
+    );
+    assert_eq!(
+        config_a
+            .cfg_repr
+            .is_feasable(sprs_problem, &config_a.cfg_repr.precompute(sprs_problem)),
+        true
+    );
+    assert_eq!(
+        config_b
+            .cfg_repr
+            .is_feasable(sprs_problem, &config_b.cfg_repr.precompute(sprs_problem)),
+        false
+    );
+    assert_eq!(
+        config_c
+            .cfg_repr
+            .is_feasable(sprs_problem, &config_c.cfg_repr.precompute(sprs_problem)),
+        false
+    );
+    assert_eq!(
+        config_d
+            .cfg_repr
+            .is_feasable(sprs_problem, &config_d.cfg_repr.precompute(sprs_problem)),
+        false
+    );
+    assert_eq!(
+        config_e
+            .cfg_repr
+            .is_feasable(sprs_problem, &config_e.cfg_repr.precompute(sprs_problem)),
+        false
+    );
+    assert_eq!(
+        config_f
+            .cfg_repr
+            .is_feasable(sprs_problem, &config_f.cfg_repr.precompute(sprs_problem)),
+        false
+    );
 }
 
 #[test]
@@ -124,10 +204,30 @@ fn test_is_feasable_no_constraint() {
     let config_3 = pb.config_from(["a", "b"]).unwrap();
 
     use crate::ilp::mat_repr::ConfigRepr;
-    assert_eq!(config_0.cfg_repr.is_feasable(&pb.pb_repr), true);
-    assert_eq!(config_1.cfg_repr.is_feasable(&pb.pb_repr), true);
-    assert_eq!(config_2.cfg_repr.is_feasable(&pb.pb_repr), true);
-    assert_eq!(config_3.cfg_repr.is_feasable(&pb.pb_repr), true);
+    assert_eq!(
+        config_0
+            .cfg_repr
+            .is_feasable(&pb.pb_repr, &config_0.cfg_repr.precompute(&pb.pb_repr)),
+        true
+    );
+    assert_eq!(
+        config_1
+            .cfg_repr
+            .is_feasable(&pb.pb_repr, &config_1.cfg_repr.precompute(&pb.pb_repr)),
+        true
+    );
+    assert_eq!(
+        config_2
+            .cfg_repr
+            .is_feasable(&pb.pb_repr, &config_2.cfg_repr.precompute(&pb.pb_repr)),
+        true
+    );
+    assert_eq!(
+        config_3
+            .cfg_repr
+            .is_feasable(&pb.pb_repr, &config_3.cfg_repr.precompute(&pb.pb_repr)),
+        true
+    );
 }
 
 #[test]
@@ -156,9 +256,9 @@ fn test_neighbours() {
 
     use crate::ilp::mat_repr::ConfigRepr;
     use std::collections::BTreeSet;
-    let neighbours = sprs_config
-        .neighbours()
+    let neighbours = (0..4)
         .into_iter()
+        .map(|x| sprs_config.neighbour(x))
         .collect::<BTreeSet<SprsConfig<String>>>();
 
     let config_0 = pb.config_from(["b"]).unwrap();
@@ -298,7 +398,9 @@ fn compute_lhs() {
 
     use crate::ilp::mat_repr::ConfigRepr;
     assert_eq!(
-        config_0.cfg_repr.compute_lhs(sprs_problem),
+        config_0
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_0.cfg_repr.precompute(sprs_problem)),
         BTreeMap::from([
             ((&a + &b).leq(&Expr::constant(1)), -1),
             ((&c + &d).leq(&Expr::constant(1)), -1),
@@ -307,7 +409,9 @@ fn compute_lhs() {
     );
     println!("{:?}", config_1.cfg_repr);
     assert_eq!(
-        config_1.cfg_repr.compute_lhs(sprs_problem),
+        config_1
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_1.cfg_repr.precompute(sprs_problem)),
         BTreeMap::from([
             ((&a + &b).leq(&Expr::constant(1)), 0),
             ((&c + &d).leq(&Expr::constant(1)), -1),
@@ -315,7 +419,9 @@ fn compute_lhs() {
         ])
     );
     assert_eq!(
-        config_2.cfg_repr.compute_lhs(sprs_problem),
+        config_2
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_2.cfg_repr.precompute(sprs_problem)),
         BTreeMap::from([
             ((&a + &b).leq(&Expr::constant(1)), 0),
             ((&c + &d).leq(&Expr::constant(1)), -1),
@@ -323,7 +429,9 @@ fn compute_lhs() {
         ])
     );
     assert_eq!(
-        config_3.cfg_repr.compute_lhs(sprs_problem),
+        config_3
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_3.cfg_repr.precompute(sprs_problem)),
         BTreeMap::from([
             ((&a + &b).leq(&Expr::constant(1)), 1),
             ((&c + &d).leq(&Expr::constant(1)), -1),
@@ -331,7 +439,9 @@ fn compute_lhs() {
         ])
     );
     assert_eq!(
-        config_4.cfg_repr.compute_lhs(sprs_problem),
+        config_4
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_4.cfg_repr.precompute(sprs_problem)),
         BTreeMap::from([
             ((&a + &b).leq(&Expr::constant(1)), -1),
             ((&c + &d).leq(&Expr::constant(1)), 0),
@@ -339,7 +449,9 @@ fn compute_lhs() {
         ])
     );
     assert_eq!(
-        config_5.cfg_repr.compute_lhs(sprs_problem),
+        config_5
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_5.cfg_repr.precompute(sprs_problem)),
         BTreeMap::from([
             ((&a + &b).leq(&Expr::constant(1)), 0),
             ((&c + &d).leq(&Expr::constant(1)), 0),
@@ -347,7 +459,9 @@ fn compute_lhs() {
         ])
     );
     assert_eq!(
-        config_6.cfg_repr.compute_lhs(sprs_problem),
+        config_6
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_6.cfg_repr.precompute(sprs_problem)),
         BTreeMap::from([
             ((&a + &b).leq(&Expr::constant(1)), 0),
             ((&c + &d).leq(&Expr::constant(1)), 0),
@@ -355,7 +469,9 @@ fn compute_lhs() {
         ])
     );
     assert_eq!(
-        config_7.cfg_repr.compute_lhs(sprs_problem),
+        config_7
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_7.cfg_repr.precompute(sprs_problem)),
         BTreeMap::from([
             ((&a + &b).leq(&Expr::constant(1)), 1),
             ((&c + &d).leq(&Expr::constant(1)), 0),
@@ -363,7 +479,9 @@ fn compute_lhs() {
         ])
     );
     assert_eq!(
-        config_8.cfg_repr.compute_lhs(sprs_problem),
+        config_8
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_8.cfg_repr.precompute(sprs_problem)),
         BTreeMap::from([
             ((&a + &b).leq(&Expr::constant(1)), -1),
             ((&c + &d).leq(&Expr::constant(1)), 0),
@@ -371,7 +489,9 @@ fn compute_lhs() {
         ])
     );
     assert_eq!(
-        config_9.cfg_repr.compute_lhs(sprs_problem),
+        config_9
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_9.cfg_repr.precompute(sprs_problem)),
         BTreeMap::from([
             ((&a + &b).leq(&Expr::constant(1)), 0),
             ((&c + &d).leq(&Expr::constant(1)), 0),
@@ -379,7 +499,9 @@ fn compute_lhs() {
         ])
     );
     assert_eq!(
-        config_a.cfg_repr.compute_lhs(sprs_problem),
+        config_a
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_a.cfg_repr.precompute(sprs_problem)),
         BTreeMap::from([
             ((&a + &b).leq(&Expr::constant(1)), 0),
             ((&c + &d).leq(&Expr::constant(1)), 0),
@@ -387,7 +509,9 @@ fn compute_lhs() {
         ])
     );
     assert_eq!(
-        config_b.cfg_repr.compute_lhs(sprs_problem),
+        config_b
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_b.cfg_repr.precompute(sprs_problem)),
         BTreeMap::from([
             ((&a + &b).leq(&Expr::constant(1)), 1),
             ((&c + &d).leq(&Expr::constant(1)), 0),
@@ -395,7 +519,9 @@ fn compute_lhs() {
         ])
     );
     assert_eq!(
-        config_c.cfg_repr.compute_lhs(sprs_problem),
+        config_c
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_c.cfg_repr.precompute(sprs_problem)),
         BTreeMap::from([
             ((&a + &b).leq(&Expr::constant(1)), -1),
             ((&c + &d).leq(&Expr::constant(1)), 1),
@@ -403,7 +529,9 @@ fn compute_lhs() {
         ])
     );
     assert_eq!(
-        config_d.cfg_repr.compute_lhs(sprs_problem),
+        config_d
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_d.cfg_repr.precompute(sprs_problem)),
         BTreeMap::from([
             ((&a + &b).leq(&Expr::constant(1)), 0),
             ((&c + &d).leq(&Expr::constant(1)), 1),
@@ -411,7 +539,9 @@ fn compute_lhs() {
         ])
     );
     assert_eq!(
-        config_e.cfg_repr.compute_lhs(sprs_problem),
+        config_e
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_e.cfg_repr.precompute(sprs_problem)),
         BTreeMap::from([
             ((&a + &b).leq(&Expr::constant(1)), 0),
             ((&c + &d).leq(&Expr::constant(1)), 1),
@@ -419,7 +549,233 @@ fn compute_lhs() {
         ])
     );
     assert_eq!(
-        config_f.cfg_repr.compute_lhs(sprs_problem),
+        config_f
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_f.cfg_repr.precompute(sprs_problem)),
+        BTreeMap::from([
+            ((&a + &b).leq(&Expr::constant(1)), 1),
+            ((&c + &d).leq(&Expr::constant(1)), 1),
+            ((&a + &d).eq(&Expr::constant(1)), 1),
+        ])
+    );
+}
+
+#[test]
+fn update_precomputation() {
+    use crate::ilp::linexpr::Expr;
+
+    let a = Expr::<String>::var("a");
+    let b = Expr::<String>::var("b");
+    let c = Expr::<String>::var("c");
+    let d = Expr::<String>::var("d");
+
+    let pb = crate::ilp::ProblemBuilder::<String, SprsProblem<_>>::new()
+        .add_variables(["a", "b", "c", "d"])
+        .unwrap()
+        .add_constraint((&a + &b).leq(&Expr::constant(1)))
+        .unwrap()
+        .add_constraint((&c + &d).leq(&Expr::constant(1)))
+        .unwrap()
+        .add_constraint((&a + &d).eq(&Expr::constant(1)))
+        .unwrap()
+        .build();
+
+    let config_0 = pb.default_config();
+    let _ = config_0.get_precomputation();
+
+    let mut config_1 = config_0.clone();
+    config_1.set("a", true).unwrap(); // ["a"]
+    let mut config_2 = config_0.clone();
+    config_2.set("b", true).unwrap(); // ["b"]
+    let mut config_3 = config_1.clone();
+    config_3.set("b", true).unwrap(); // ["a", "b"]
+    let mut config_4 = config_2.clone();
+    config_4.set("b", false).unwrap();
+    config_4.set("c", true).unwrap(); // ["c"]
+    let mut config_5 = config_4.clone();
+    config_5.set("a", true).unwrap(); // ["a","c"]
+    let mut config_6 = config_4.clone();
+    config_6.set("b", true).unwrap(); // ["b","c"]
+    let mut config_7 = config_6.clone();
+    config_7.set("a", true).unwrap(); // ["a","b","c"]
+
+    let mut config_8 = config_0.clone();
+    config_8.set("d", true).unwrap(); // ["d"]
+
+    let mut config_9 = config_8.clone();
+    config_9.set("a", true).unwrap(); // ["a","d"]
+    let mut config_a = config_8.clone();
+    config_a.set("b", true).unwrap(); // ["b","d"]
+    let mut config_b = config_9.clone();
+    config_b.set("b", true).unwrap(); // ["a", "b","d"]
+    let mut config_c = config_a.clone();
+    config_c.set("b", false).unwrap();
+    config_c.set("c", true).unwrap(); // ["c","d"]
+    let mut config_d = config_c.clone();
+    config_d.set("a", true).unwrap(); // ["a","c","d"]
+    let mut config_e = config_c.clone();
+    config_e.set("b", true).unwrap(); // ["b","c","d"]
+    let mut config_f = config_e.clone();
+    config_f.set("a", true).unwrap(); // ["a","b","c","d"]
+
+    let sprs_problem = &pb.pb_repr;
+
+    use crate::ilp::mat_repr::ConfigRepr;
+    assert_eq!(
+        config_0
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_0.get_precomputation()),
+        BTreeMap::from([
+            ((&a + &b).leq(&Expr::constant(1)), -1),
+            ((&c + &d).leq(&Expr::constant(1)), -1),
+            ((&a + &d).eq(&Expr::constant(1)), -1),
+        ])
+    );
+    println!("{:?}", config_1.cfg_repr);
+    assert_eq!(
+        config_1
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_1.get_precomputation()),
+        BTreeMap::from([
+            ((&a + &b).leq(&Expr::constant(1)), 0),
+            ((&c + &d).leq(&Expr::constant(1)), -1),
+            ((&a + &d).eq(&Expr::constant(1)), 0),
+        ])
+    );
+    assert_eq!(
+        config_2
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_2.get_precomputation()),
+        BTreeMap::from([
+            ((&a + &b).leq(&Expr::constant(1)), 0),
+            ((&c + &d).leq(&Expr::constant(1)), -1),
+            ((&a + &d).eq(&Expr::constant(1)), -1),
+        ])
+    );
+    assert_eq!(
+        config_3
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_3.get_precomputation()),
+        BTreeMap::from([
+            ((&a + &b).leq(&Expr::constant(1)), 1),
+            ((&c + &d).leq(&Expr::constant(1)), -1),
+            ((&a + &d).eq(&Expr::constant(1)), 0),
+        ])
+    );
+    assert_eq!(
+        config_4
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_4.get_precomputation()),
+        BTreeMap::from([
+            ((&a + &b).leq(&Expr::constant(1)), -1),
+            ((&c + &d).leq(&Expr::constant(1)), 0),
+            ((&a + &d).eq(&Expr::constant(1)), -1),
+        ])
+    );
+    assert_eq!(
+        config_5
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_5.get_precomputation()),
+        BTreeMap::from([
+            ((&a + &b).leq(&Expr::constant(1)), 0),
+            ((&c + &d).leq(&Expr::constant(1)), 0),
+            ((&a + &d).eq(&Expr::constant(1)), 0),
+        ])
+    );
+    assert_eq!(
+        config_6
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_6.get_precomputation()),
+        BTreeMap::from([
+            ((&a + &b).leq(&Expr::constant(1)), 0),
+            ((&c + &d).leq(&Expr::constant(1)), 0),
+            ((&a + &d).eq(&Expr::constant(1)), -1),
+        ])
+    );
+    assert_eq!(
+        config_7
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_7.get_precomputation()),
+        BTreeMap::from([
+            ((&a + &b).leq(&Expr::constant(1)), 1),
+            ((&c + &d).leq(&Expr::constant(1)), 0),
+            ((&a + &d).eq(&Expr::constant(1)), 0),
+        ])
+    );
+    assert_eq!(
+        config_8
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_8.get_precomputation()),
+        BTreeMap::from([
+            ((&a + &b).leq(&Expr::constant(1)), -1),
+            ((&c + &d).leq(&Expr::constant(1)), 0),
+            ((&a + &d).eq(&Expr::constant(1)), 0),
+        ])
+    );
+    assert_eq!(
+        config_9
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_9.get_precomputation()),
+        BTreeMap::from([
+            ((&a + &b).leq(&Expr::constant(1)), 0),
+            ((&c + &d).leq(&Expr::constant(1)), 0),
+            ((&a + &d).eq(&Expr::constant(1)), 1),
+        ])
+    );
+    assert_eq!(
+        config_a
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_a.get_precomputation()),
+        BTreeMap::from([
+            ((&a + &b).leq(&Expr::constant(1)), 0),
+            ((&c + &d).leq(&Expr::constant(1)), 0),
+            ((&a + &d).eq(&Expr::constant(1)), 0),
+        ])
+    );
+    assert_eq!(
+        config_b
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_b.get_precomputation()),
+        BTreeMap::from([
+            ((&a + &b).leq(&Expr::constant(1)), 1),
+            ((&c + &d).leq(&Expr::constant(1)), 0),
+            ((&a + &d).eq(&Expr::constant(1)), 1),
+        ])
+    );
+    assert_eq!(
+        config_c
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_c.get_precomputation()),
+        BTreeMap::from([
+            ((&a + &b).leq(&Expr::constant(1)), -1),
+            ((&c + &d).leq(&Expr::constant(1)), 1),
+            ((&a + &d).eq(&Expr::constant(1)), 0),
+        ])
+    );
+    assert_eq!(
+        config_d
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_d.get_precomputation()),
+        BTreeMap::from([
+            ((&a + &b).leq(&Expr::constant(1)), 0),
+            ((&c + &d).leq(&Expr::constant(1)), 1),
+            ((&a + &d).eq(&Expr::constant(1)), 1),
+        ])
+    );
+    assert_eq!(
+        config_e
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_e.get_precomputation()),
+        BTreeMap::from([
+            ((&a + &b).leq(&Expr::constant(1)), 0),
+            ((&c + &d).leq(&Expr::constant(1)), 1),
+            ((&a + &d).eq(&Expr::constant(1)), 0),
+        ])
+    );
+    assert_eq!(
+        config_f
+            .cfg_repr
+            .compute_lhs(sprs_problem, &config_f.get_precomputation()),
         BTreeMap::from([
             ((&a + &b).leq(&Expr::constant(1)), 1),
             ((&c + &d).leq(&Expr::constant(1)), 1),
