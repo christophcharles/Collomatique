d9535ca22187014b769116f2fe9287acc15acba3
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2024-07-02 21:02:43 +0200

    Add group_lists_add

--- src/backend/sqlite/group_lists.rs
+++ src/backend/sqlite/group_lists.rs
@@ -154,14 +154,98 @@ WHERE group_list_id = ?
     Ok(output)
 }
 
+async fn search_invalid_student_id(
+    pool: &SqlitePool,
+    group_list: &GroupList<super::students::Id>,
+) -> Result<Option<super::students::Id>> {
+    let students_id = sqlx::query!("SELECT student_id FROM students",)
+        .fetch_all(pool)
+        .await
+        .map_err(Error::from)?
+        .iter()
+        .map(|x| x.student_id)
+        .collect::<BTreeSet<_>>();
+
+    for (student, _group) in &group_list.students_mapping {
+        if !students_id.contains(&student.0) {
+            return Ok(Some(*student));
+        }
+    }
+
+    Ok(None)
+}
+
+fn validate_groups(group_list: &GroupList<super::students::Id>) -> bool {
+    for (_student, &group) in &group_list.students_mapping {
+        if group >= group_list.groups.len() {
+            return false;
+        }
+    }
+    true
+}
+
 pub async fn add(
-    _pool: &SqlitePool,
-    _group_list: &GroupList<super::students::Id>,
+    pool: &SqlitePool,
+    group_list: &GroupList<super::students::Id>,
 ) -> std::result::Result<
     Id,
     InvalidCrossError<Error, GroupList<super::students::Id>, super::students::Id>,
 > {
-    todo!()
+    if let Some(student_id) = search_invalid_student_id(pool, group_list).await? {
+        return Err(InvalidCrossError::InvalidCrossId(student_id));
+    }
+    if !validate_groups(group_list) {
+        return Err(InvalidCrossError::InvalidData(group_list.clone()));
+    }
+
+    let mut conn = pool.acquire().await.map_err(Error::from)?;
+
+    let group_list_id = sqlx::query!("INSERT INTO group_lists (name) VALUES (?)", group_list.name,)
+        .execute(&mut *conn)
+        .await
+        .map_err(Error::from)?
+        .last_insert_rowid();
+
+    let mut group_ids = Vec::with_capacity(group_list.groups.len());
+    for group in &group_list.groups {
+        let extendable = if group.extendable { 1 } else { 0 };
+        let group_id = sqlx::query!(
+            "INSERT INTO groups (name, extendable) VALUES (?1, ?2)",
+            group.name,
+            extendable,
+        )
+        .execute(&mut *conn)
+        .await
+        .map_err(Error::from)?
+        .last_insert_rowid();
+
+        group_ids.push(group_id);
+
+        let _ = sqlx::query!(
+            "INSERT INTO group_list_items (group_list_id, group_id) VALUES (?1, ?2)",
+            group_list_id,
+            group_id,
+        )
+        .execute(&mut *conn)
+        .await
+        .map_err(Error::from)?;
+    }
+
+    for (&student_id, &group_num) in &group_list.students_mapping {
+        let group_id = group_ids[group_num];
+
+        let _ = sqlx::query!(
+            "INSERT INTO group_items (group_list_id, group_id, student_id) VALUES (?1, ?2, ?3)",
+            group_list_id,
+            group_id,
+            student_id.0
+        )
+        .execute(&mut *conn)
+        .await
+        .map_err(Error::from)?;
+    }
+
+    Ok(Id(group_list_id))
 }
 
 pub async fn remove(_pool: &SqlitePool, _index: Id) -> std::result::Result<(), IdError<Error, Id>> {
