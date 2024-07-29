bd0261ba02979041b9f618ec22ab556fec4b3b2a
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2024-07-29 11:32:53 +0200

    Move to execution of module function rather than directly a file

--- src/frontend/python.rs
+++ src/frontend/python.rs
@@ -1,14 +1,18 @@
+use std::path::PathBuf;
+
 use pyo3::prelude::*;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct PythonCode {
     code: String,
+    file: PathBuf,
 }
 
 impl PythonCode {
     pub fn from_code(code: &str) -> Self {
         PythonCode {
             code: code.to_string(),
+            file: PathBuf::new(),
         }
     }
 
@@ -18,6 +22,7 @@ impl PythonCode {
 
         let mut python_code = PythonCode {
             code: String::new(),
+            file: path.to_path_buf(),
         };
 
         let mut file = File::open(path)?;
@@ -26,8 +31,26 @@ impl PythonCode {
         Ok(python_code)
     }
 
-    pub fn run(&self) -> PyResult<()> {
-        Python::with_gil(|py| py.run_bound(&self.code, None, None))
+    pub fn run(&self, func: &str) -> PyResult<()> {
+        Python::with_gil(|py| {
+            let python_code = PyModule::from_code_bound(
+                py,
+                &self.code,
+                &self.file.to_string_lossy(),
+                &self
+                    .file
+                    .as_path()
+                    .file_stem()
+                    .unwrap_or_default()
+                    .to_string_lossy(),
+            )?;
+
+            let fun: Py<PyAny> = python_code.getattr(func)?.into();
+
+            fun.call0(py)?;
+
+            Ok(())
+        })
     }
 }
 
