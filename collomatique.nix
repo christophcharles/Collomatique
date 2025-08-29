54d89987689525b5eeab342986c9be222977814e
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:15 +0200

    Simplify nix files as we do not need iced anymore

--- collomatique.nix
+++ collomatique.nix
@@ -1,21 +1,8 @@
 {
     rustPlatform,
     lib,
-    sqlite,
     cbc,
-    cmake,
-    python3,
-    fontconfig,
-    freetype,
-    libglvnd,
-    libinput,
-    libxkbcommon,
-    makeBinaryWrapper,
-    mesa,
     pkg-config,
-    vulkan-loader,
-    wayland,
-    xorg
 }:
 rustPlatform.buildRustPackage rec {
     pname = "collomatique";
@@ -28,51 +15,13 @@ rustPlatform.buildRustPackage rec {
     };
 
     nativeBuildInputs = [
-        cmake
         rustPlatform.bindgenHook
-        python3
-        pkg-config
-        makeBinaryWrapper
         cbc # We need it for tests
     ];
 
     buildInputs = [
-        sqlite
         cbc
-        python3
-        fontconfig
-        freetype
-        libglvnd
-        libinput
-        libxkbcommon
-        vulkan-loader
-        wayland
-        xorg.libX11
     ];
-
-    # Force linking to libEGL, which is always dlopen()ed, and to
-    # libwayland-client, which is always dlopen()ed except by the
-    # obscure winit backend.
-    RUSTFLAGS = map (a: "-C link-arg=${a}") [
-        "-Wl,--push-state,--no-as-needed"
-        "-lEGL"
-        "-lwayland-client"
-        "-Wl,--pop-state"
-    ];
-
-    shellHook = ''
-        export LD_LIBRARY_PATH="${lib.makeLibraryPath [
-            xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr vulkan-loader libxkbcommon mesa.drivers wayland
-            ]}:$LD_LIBRARY_PATH"
-    '';
-
-    # LD_LIBRARY_PATH can be removed once tiny-xlib is bumped above 0.2.2
-    postInstall = ''
-        wrapProgram "$out/bin/${pname}" \
-            --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [
-            xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr vulkan-loader libxkbcommon mesa.drivers wayland
-            ]}
-    '';
 
     meta = {
         description = "Automatic colloscope building program";
