c06b0ac6bf2fe421891a194e8d6708e5cec52f12
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:14 +0200

    Add stub structs for colloscopes

---
+++ core/src/colloscopes/structs.rs
@@ -0,0 +1,36 @@
+//! Definition of relevant structures to describe colloscopes
+//! 
+//! This module contains the basic colloscope structures.
+//! The main such structure is [Colloscope] which describes
+//! a (partially completed or not) colloscope.
+
+use std::collections::{HashMap, HashSet};
+use std::num::NonZeroUsize;
+
+pub trait Identifier : Clone + Copy + std::fmt::Debug + Ord + PartialOrd + Eq + PartialEq + std::hash::Hash + Send + Sync {}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct GroupList<StudentId: Identifier> {
+    students_per_group: std::ops::RangeInclusive<usize>,
+    groups: Vec<HashSet<StudentId>>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct TeacherInterrogations {
+
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct SubjectInterrogations<TeacherId: Identifier, GroupListId: Identifier> {
+    duration: crate::time::NonZeroDurationInMinutes,
+    period: NonZeroUsize,
+    group_list_id: GroupListId,
+    teacher_map: HashMap<TeacherId, TeacherInterrogations>
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Colloscope<SubjectId: Identifier, TeacherId: Identifier, GroupListId: Identifier, StudentId: Identifier> {
+    subject_map: HashMap<SubjectId, SubjectInterrogations<TeacherId, GroupListId>>,
+    group_lists: HashMap<GroupListId, GroupList<StudentId>>,
+}
+
