af03766bd9e650a3cc8c84562a6defba98279a7c
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:17 +0200

    Add first python command

--- core/src/python/glue.rs
+++ core/src/python/glue.rs
@@ -2,7 +2,10 @@ use pyo3::prelude::*;
 
 #[pymodule]
 pub fn collomatique(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<Session>()?;
+
     m.add_function(wrap_pyfunction!(log, m)?)?;
+    m.add_function(wrap_pyfunction!(current_session, m)?)?;
 
     Ok(())
 }
@@ -12,4 +15,21 @@ pub fn log(msg: String) {
     use std::io::Write;
     eprint!("{}\r\n", msg);
     std::io::stderr().flush().expect("no error on flush");
+}
+
+#[pyfunction]
+pub fn current_session() -> Session {
+    Session {}
+}
+
+#[pyclass]
+pub struct Session {}
+
+mod general_planning;
+
+#[pymethods]
+impl Session {
+    fn periods(_self: PyRef<'_, Self>) -> general_planning::SessionPeriods {
+        general_planning::SessionPeriods {}
+    }
 }
