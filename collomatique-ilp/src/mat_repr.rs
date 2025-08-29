ac072666d93806412dc651fdda842086030a5be5
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:13 +0200

    Add basic nd documentation

--- collomatique-ilp/src/mat_repr.rs
+++ collomatique-ilp/src/mat_repr.rs
@@ -18,27 +18,63 @@ use super::{Constraint, UsableData, Variable};
 
 use std::collections::BTreeMap;
 
+/// Matrix representation of a problem
+/// 
+/// This trait is implemented by internal matrix representation of problems.
+/// Indeed, in order to make computations (most notably to check if a configuration
+/// is feasable), [crate::Problem] uses an internal representation, usually a matrix
+/// representation.
+/// 
+/// This trait must be implemented by any such internal representation.
+/// 
+/// The default representation if you don't specify it is given by [crate::DefaultRepr].
 pub trait ProblemRepr<V: UsableData>:
     Clone + std::fmt::Debug + Send + Sync + PartialEq + Eq + PartialOrd + Ord
 {
+    /// The corresponding representation for configurations.
+    /// 
+    /// A configuration must always point to its parent [ProblemRepr].
     type Config<'a>: ConfigRepr<'a, V>
     where
         Self: 'a;
 
+    /// Builds a new representation from variables description
+    /// and iterator over constraints.
     fn new<'a, T>(variables: &BTreeMap<V, Variable>, constraints: T) -> Self
     where
         V: 'a,
         T: ExactSizeIterator<Item = &'a Constraint<V>>;
+
+    /// Builds the representation for a specific configuration
+    /// defined by a map between variables and their values.
     fn config_from<'a>(
         &'a self,
         vars: &BTreeMap<V, ordered_float::OrderedFloat<f64>>,
     ) -> Self::Config<'a>;
 }
 
+/// Matrix representation of a configuration
+/// 
+/// This trait is implemented by internal matrix representation of configurations ([crate::Config]).
+/// This is the configuration equivalent to [ProblemRepr].
+/// 
+/// Each [ConfigRepr] is associated with a [ProblemRepr] through [ProblemRepr::Config].
+
 pub trait ConfigRepr<'a, V: UsableData>:
     PartialEq + Eq + PartialOrd + Ord + Sized + Clone + std::fmt::Debug + Send + Sync
 {
+    /// Returns the list of unsatisfied constraints by the current configuration
+    /// for its parent [ProblemRepr].
+    /// 
+    /// The list can be empty if all the constraints are satisfied.
     fn unsatisfied_constraints(&self) -> Vec<usize>;
+
+    /// Returns true if the configuration is feasable.
+    /// 
+    /// Here, the definition of feasable is restricted:
+    /// we do not have to check the domain of the different variables.
+    /// 
+    /// The default implementation uses [ConfigRepr::unsatisfied_constraints].
     fn is_feasable(&self) -> bool {
         let unsatisfied_constraints = self.unsatisfied_constraints();
 
