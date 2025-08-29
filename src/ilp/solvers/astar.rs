25d2255f7f89b6f14e36c03aa1c323bdd9fef693
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:33:26 +0200

    First naive implementation of A*

--- src/ilp/solvers/astar.rs
+++ src/ilp/solvers/astar.rs
@@ -3,6 +3,7 @@ mod tests;
 
 use crate::ilp::ndtools::ConfigRepr;
 use crate::ilp::{Config, FeasableConfig, Problem};
+use std::collections::{BTreeMap, BTreeSet};
 
 #[derive(Debug, Clone)]
 pub struct Solver<'a> {
@@ -17,10 +18,19 @@ impl<'a> Solver<'a> {
     fn distance_heuristic(&self, _config: &ConfigRepr) -> f32 {
         0.
     }
+
+    fn min_f_score(
+        open_nodes: &mut BTreeSet<ConfigRepr<'a>>,
+        f_scores: &BTreeMap<ConfigRepr<'a>, f32>,
+    ) -> Option<ConfigRepr<'a>> {
+        use ordered_float::OrderedFloat;
+
+        let (min_config, _) = f_scores.iter().min_by_key(|(_, s)| OrderedFloat(**s))?;
+        open_nodes.take(min_config)
+    }
 }
 
 use super::FeasabilitySolver;
-use std::collections::BTreeSet;
 
 impl<'a> FeasabilitySolver<'a> for Solver<'a> {
     fn restore_feasability_exclude(
@@ -29,26 +39,38 @@ impl<'a> FeasabilitySolver<'a> for Solver<'a> {
         exclude_list: &BTreeSet<&FeasableConfig>,
     ) -> Option<FeasableConfig<'a>> {
         let config_repr = self.problem.mat_repr.config(config);
-
-        use std::collections::VecDeque;
+        let init_g_score = 0.0f32;
+        let init_f_score = init_g_score + self.distance_heuristic(&config_repr);
 
         let exclude_configs: BTreeSet<ConfigRepr<'_>> =
             exclude_list.iter().map(|x| x.inner().repr()).collect();
-        let mut explored_configs = exclude_configs.clone();
-        let mut config_queue = VecDeque::new();
-        config_queue.push_back(config_repr);
 
-        while let Some(candidate) = config_queue.pop_front() {
+        let mut g_scores = BTreeMap::from([(config_repr.clone(), init_g_score)]);
+        let mut f_scores = BTreeMap::from([(config_repr.clone(), init_f_score)]);
+
+        let mut open_nodes = BTreeSet::from([config_repr]);
+
+        while let Some(candidate) = Self::min_f_score(&mut open_nodes, &f_scores) {
             if candidate.is_feasable() && !exclude_configs.contains(&candidate) {
                 return Some(unsafe { Config::from(candidate).into_feasable_unchecked() });
             } else {
-                config_queue.extend(
-                    candidate
-                        .neighbours()
-                        .into_iter()
-                        .filter(|x| !explored_configs.contains(x)),
-                );
-                explored_configs.insert(candidate);
+                let candidate_g_score = g_scores
+                    .get(&candidate)
+                    .copied()
+                    .expect("Score should be computed for current node");
+                for neighbour in candidate.neighbours() {
+                    let tentative_g_score = candidate_g_score + 1.0f32; // neighbours are always at a distance 1 for the currently considered node
+
+                    let neighbour_g_score =
+                        g_scores.get(&neighbour).copied().unwrap_or(f32::INFINITY);
+                    if tentative_g_score < neighbour_g_score {
+                        let tentative_f_score =
+                            tentative_g_score + self.distance_heuristic(&neighbour);
+                        f_scores.insert(neighbour.clone(), tentative_f_score);
+                        g_scores.insert(neighbour.clone(), tentative_g_score);
+                        open_nodes.insert(neighbour);
+                    }
+                }
             }
         }
 
