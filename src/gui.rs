dbc95a3dd36f557c96a98c06ae2fa23bf6af7e59
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:33:11 +0200

    Add basic empty window

--- src/gui.rs
+++ src/gui.rs
@@ -1,11 +1,30 @@
 use anyhow::Result;
 
-pub fn run_gui(create: bool, db: Option<std::path::PathBuf>) -> Result<()> {
-    println!("GUI was run with file: {:?}", db);
-    if create {
-        println!("File should be created (and non-existent)");
-    } else {
-        println!("File should already exist");
+use iced::widget::text;
+use iced::Element;
+
+struct GuiState {
+    txt: String,
+}
+
+impl Default for GuiState {
+    fn default() -> Self {
+        Self {
+            txt: String::from("Placeholder"),
+        }
     }
+}
+
+#[derive(Debug, Clone)]
+enum Message {}
+
+fn update(_state: &mut GuiState, _message: Message) {}
+
+fn view(state: &GuiState) -> Element<Message> {
+    text(&state.txt).into()
+}
+
+pub fn run_gui(_create: bool, _db: Option<std::path::PathBuf>) -> Result<()> {
+    iced::run("Collomatique", update, view)?;
     Ok(())
 }
