483d13a07428b7b5d7474bb994414ebc248202a9
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:32:12 +0200

    Create basic source tree with nix compilation files

---
+++ collomatique.nix
@@ -0,0 +1,19 @@
+{
+    rustPlatform,
+    lib,
+}:
+rustPlatform.buildRustPackage {
+    pname = "collomatique";
+    version = "0.1.0";
+
+    src = [ ./. ];
+
+    cargoLock = {
+        lockFile = ./Cargo.lock;
+    };
+
+    meta = {
+        description = "Automatic colloscope building program";
+        license = lib.licenses.agpl3;
+    };
+}
