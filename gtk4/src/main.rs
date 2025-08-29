0938817329bc635147d44b74bd60adf7794b5ffa
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:15 +0200

    Add core and gtk4 stub

---
+++ gtk4/src/main.rs
@@ -0,0 +1,8 @@
+//! Collomatique GTK4 main executable
+//!
+//! At this date, the goal of this code is to be a gtk4 GUI
+//! for the collomatique-core crate.
+
+fn main() {
+    println!("Hello, world!");
+}
