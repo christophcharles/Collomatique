2bfc6ec8817b68952250e5b8dd811a20f0a3d50e
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:33:31 +0200

    Add tests for add in time_slots

--- src/backend/sqlite/tests/time_slots.rs
+++ src/backend/sqlite/tests/time_slots.rs
@@ -1460,3 +1460,342 @@ async fn time_slots_update(pool: sqlx::SqlitePool) {
 
     assert_eq!(time_slots, time_slots_expected);
 }
+
+#[sqlx::test]
+async fn time_slots_add_one(pool: sqlx::SqlitePool) {
+    let store = prepare_example_db(pool).await;
+
+    let id = store
+        .time_slots_add(&TimeSlot {
+            subject_id: super::super::subjects::Id(3),
+            teacher_id: super::super::teachers::Id(4),
+            start: SlotStart {
+                day: crate::time::Weekday::Wednesday,
+                time: crate::time::Time::from_hm(8, 0).unwrap(),
+            },
+            week_pattern_id: super::super::week_patterns::Id(3),
+            room: String::from("Test"),
+        })
+        .await
+        .unwrap();
+    assert_eq!(id, super::super::time_slots::Id(24));
+
+    let time_slots = store.time_slots_get_all().await.unwrap();
+
+    let time_slots_expected = BTreeMap::from([
+        (
+            super::super::time_slots::Id(1),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(1),
+                teacher_id: super::super::teachers::Id(1),
+                start: SlotStart {
+                    day: crate::time::Weekday::Thursday,
+                    time: crate::time::Time::from_hm(16, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(1),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(2),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(1),
+                teacher_id: super::super::teachers::Id(1),
+                start: SlotStart {
+                    day: crate::time::Weekday::Thursday,
+                    time: crate::time::Time::from_hm(17, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(2),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(3),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(2),
+                teacher_id: super::super::teachers::Id(2),
+                start: SlotStart {
+                    day: crate::time::Weekday::Tuesday,
+                    time: crate::time::Time::from_hm(14, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(1),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(4),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(2),
+                teacher_id: super::super::teachers::Id(2),
+                start: SlotStart {
+                    day: crate::time::Weekday::Tuesday,
+                    time: crate::time::Time::from_hm(16, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(1),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(5),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(2),
+                teacher_id: super::super::teachers::Id(3),
+                start: SlotStart {
+                    day: crate::time::Weekday::Thursday,
+                    time: crate::time::Time::from_hm(16, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(1),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(6),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(2),
+                teacher_id: super::super::teachers::Id(4),
+                start: SlotStart {
+                    day: crate::time::Weekday::Thursday,
+                    time: crate::time::Time::from_hm(17, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(1),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(7),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(3),
+                teacher_id: super::super::teachers::Id(5),
+                start: SlotStart {
+                    day: crate::time::Weekday::Monday,
+                    time: crate::time::Time::from_hm(14, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(1),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(8),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(3),
+                teacher_id: super::super::teachers::Id(5),
+                start: SlotStart {
+                    day: crate::time::Weekday::Thursday,
+                    time: crate::time::Time::from_hm(16, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(1),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(9),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(3),
+                teacher_id: super::super::teachers::Id(6),
+                start: SlotStart {
+                    day: crate::time::Weekday::Thursday,
+                    time: crate::time::Time::from_hm(16, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(1),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(10),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(3),
+                teacher_id: super::super::teachers::Id(7),
+                start: SlotStart {
+                    day: crate::time::Weekday::Wednesday,
+                    time: crate::time::Time::from_hm(17, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(1),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(11),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(4),
+                teacher_id: super::super::teachers::Id(8),
+                start: SlotStart {
+                    day: crate::time::Weekday::Monday,
+                    time: crate::time::Time::from_hm(15, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(1),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(12),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(4),
+                teacher_id: super::super::teachers::Id(9),
+                start: SlotStart {
+                    day: crate::time::Weekday::Monday,
+                    time: crate::time::Time::from_hm(15, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(1),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(13),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(4),
+                teacher_id: super::super::teachers::Id(10),
+                start: SlotStart {
+                    day: crate::time::Weekday::Wednesday,
+                    time: crate::time::Time::from_hm(17, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(1),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(14),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(4),
+                teacher_id: super::super::teachers::Id(11),
+                start: SlotStart {
+                    day: crate::time::Weekday::Thursday,
+                    time: crate::time::Time::from_hm(17, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(1),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(15),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(7),
+                teacher_id: super::super::teachers::Id(12),
+                start: SlotStart {
+                    day: crate::time::Weekday::Monday,
+                    time: crate::time::Time::from_hm(14, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(1),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(16),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(7),
+                teacher_id: super::super::teachers::Id(13),
+                start: SlotStart {
+                    day: crate::time::Weekday::Monday,
+                    time: crate::time::Time::from_hm(14, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(1),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(17),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(7),
+                teacher_id: super::super::teachers::Id(14),
+                start: SlotStart {
+                    day: crate::time::Weekday::Tuesday,
+                    time: crate::time::Time::from_hm(16, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(1),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(18),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(7),
+                teacher_id: super::super::teachers::Id(14),
+                start: SlotStart {
+                    day: crate::time::Weekday::Tuesday,
+                    time: crate::time::Time::from_hm(17, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(1),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(19),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(5),
+                teacher_id: super::super::teachers::Id(15),
+                start: SlotStart {
+                    day: crate::time::Weekday::Monday,
+                    time: crate::time::Time::from_hm(14, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(1),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(20),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(5),
+                teacher_id: super::super::teachers::Id(16),
+                start: SlotStart {
+                    day: crate::time::Weekday::Monday,
+                    time: crate::time::Time::from_hm(15, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(1),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(21),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(5),
+                teacher_id: super::super::teachers::Id(17),
+                start: SlotStart {
+                    day: crate::time::Weekday::Friday,
+                    time: crate::time::Time::from_hm(17, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(1),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(22),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(6),
+                teacher_id: super::super::teachers::Id(18),
+                start: SlotStart {
+                    day: crate::time::Weekday::Thursday,
+                    time: crate::time::Time::from_hm(13, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(1),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(23),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(8),
+                teacher_id: super::super::teachers::Id(14),
+                start: SlotStart {
+                    day: crate::time::Weekday::Wednesday,
+                    time: crate::time::Time::from_hm(16, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(1),
+                room: String::from(""),
+            },
+        ),
+        (
+            super::super::time_slots::Id(24),
+            TimeSlot {
+                subject_id: super::super::subjects::Id(3),
+                teacher_id: super::super::teachers::Id(4),
+                start: SlotStart {
+                    day: crate::time::Weekday::Wednesday,
+                    time: crate::time::Time::from_hm(8, 0).unwrap(),
+                },
+                week_pattern_id: super::super::week_patterns::Id(3),
+                room: String::from("Test"),
+            },
+        ),
+    ]);
+
+    assert_eq!(time_slots, time_slots_expected);
+}
