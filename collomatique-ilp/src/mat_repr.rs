c4f5865d66d1229297dbc4dd64ebcdc00c4d0f67
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:12 +0200

    Start implementation of ConfigData Add mat_repr module

---
+++ collomatique-ilp/src/mat_repr.rs
@@ -0,0 +1,14 @@
+//! Matrix representation of problems and configurations
+//! 
+//! This module contains the definition of two traits [ProblemRepr] and [ConfigRepr].
+//! These traits are used to represent ILP problem and corresponding configurations into
+//! some actual numerical representation, usually matrix repr.
+//! 
+//! In addition to these traits, the module contains two submodules [nd] and [sparse]
+//! which contain the implementation using the ndarray crate and a sparse matrix representation
+//! respectively.
+//! 
+//! Technically, it should be possible to represent problems and configurations using something else
+//! than matrices. But this is the straightforward way to do it and only this way was indeed
+//! implemented.
+
