dc50a7f165a1afdde5c7979200754076724c31e4
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:14 +0200

    Add colloscopes module (stub) and a bit of documentation

---
+++ core/src/colloscopes.rs
@@ -0,0 +1,5 @@
+//! Colloscopes module
+//! 
+//! This modules contain the code that defines the various colloscopes constraints.
+//! It builds on [crate::generics] and [crate::solver] and uses its interfaces
+//! to build a *nice-enough* interface for automatic solving of colloscopes.
