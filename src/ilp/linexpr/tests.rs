cdb2db0f38665f6cefc76d929adcd842519c2bc1
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:33:26 +0200

    Remove Into trait for Expr and replace by specialized var and constant constructor function

--- src/ilp/linexpr/tests.rs
+++ src/ilp/linexpr/tests.rs
@@ -4,19 +4,19 @@ use std::collections::BTreeMap;
 
 #[test]
 fn expr_display() {
-    let expr = 2 * Expr::from("a") - 3 * Expr::from("b") + 4 * Expr::from("c");
+    let expr = 2 * Expr::var("a") - 3 * Expr::var("b") + 4 * Expr::var("c");
     assert_eq!(format!("{}", expr), "2*a + (-3)*b + 4*c");
 
-    let expr = 2 * Expr::from("a") - 3 * Expr::from("b") + 4 * Expr::from("c") + 1;
+    let expr = 2 * Expr::var("a") - 3 * Expr::var("b") + 4 * Expr::var("c") + 1;
     assert_eq!(format!("{}", expr), "2*a + (-3)*b + 4*c + 1");
 
-    let expr = 2 * Expr::from("a") - 3 * Expr::from("b") + 4 * Expr::from("c") - 2;
+    let expr = 2 * Expr::var("a") - 3 * Expr::var("b") + 4 * Expr::var("c") - 2;
     assert_eq!(format!("{}", expr), "2*a + (-3)*b + 4*c + (-2)");
 
-    let expr = Expr::from(3);
+    let expr = Expr::constant(3);
     assert_eq!(format!("{}", expr), "3");
 
-    let expr = Expr::from(-42);
+    let expr = Expr::constant(-42);
     assert_eq!(format!("{}", expr), "(-42)");
 }
 
@@ -80,22 +80,22 @@ fn expr_sub() {
 
 #[test]
 fn expr_mul() {
-    let expr1 = -2* Expr::from("a") + 3 *  Expr::from("b") + 2;
-    let expr2 = -4 * Expr::from("a") + 6* Expr::from("b") + 4;
+    let expr1 = -2* Expr::var("a") + 3 *  Expr::var("b") + 2;
+    let expr2 = -4 * Expr::var("a") + 6* Expr::var("b") + 4;
     assert_eq!(2*expr1, expr2);
 
-    let expr1 = -2* Expr::from("a") + 3 *  Expr::from("b") + 2;
-    let expr2 = 6 * Expr::from("a") - 9 * Expr::from("b") - 6;
+    let expr1 = -2* Expr::var("a") + 3 *  Expr::var("b") + 2;
+    let expr2 = 6 * Expr::var("a") - 9 * Expr::var("b") - 6;
     assert_eq!((-3)*expr1, expr2);
 
-    let expr1 = -2* Expr::from("a") + 3 *  Expr::from("b") + 2;
-    let expr2 = Expr::from(0);
+    let expr1 = -2* Expr::var("a") + 3 *  Expr::var("b") + 2;
+    let expr2 = Expr::constant(0);
     assert_eq!(0*expr1, expr2);
 }
 
 #[test]
 fn expr_reduce() {
-    let expr1 = -2* Expr::from("a") + 3 *  Expr::from("b") - 4 * Expr::from("c") + 1;
+    let expr1 = -2* Expr::var("a") + 3 *  Expr::var("b") - 4 * Expr::var("c") + 1;
 
     let config = Config {
         values: BTreeMap::from([
@@ -104,15 +104,15 @@ fn expr_reduce() {
         ]),
     };
 
-    let expr2 = 3 * Expr::from("b") - 1;
+    let expr2 = 3 * Expr::var("b") - 1;
 
     assert_eq!(expr1.reduce(&config), expr2);
 }
 
 #[test]
 fn expr_to_value() {
-    let expr1 = -2* Expr::from("a") + 3 *  Expr::from("b") - 4 * Expr::from("c") + 1;
-    let expr2 = Expr::from(42);
+    let expr1 = -2* Expr::var("a") + 3 *  Expr::var("b") - 4 * Expr::var("c") + 1;
+    let expr2 = Expr::constant(42);
 
     assert_eq!(expr1.to_value(), None);
     assert_eq!(expr2.to_value(), Some(42));
@@ -120,8 +120,8 @@ fn expr_to_value() {
 
 #[test]
 fn expr_eval() {
-    let expr1 = -2* Expr::from("a") + 3 *  Expr::from("b") - 4 * Expr::from("c") + 1;
-    let expr2 = -2* Expr::from("a") - 4 * Expr::from("c") + 1;
+    let expr1 = -2* Expr::var("a") + 3 *  Expr::var("b") - 4 * Expr::var("c") + 1;
+    let expr2 = -2* Expr::var("a") - 4 * Expr::var("c") + 1;
 
     let config = Config {
         values: BTreeMap::from([
@@ -136,12 +136,12 @@ fn expr_eval() {
 
 #[test]
 fn constraint_eval() {
-    let expr1 = -2* Expr::from("a") + 3 *  Expr::from("b") - 4 * Expr::from("c") + 1;
-    let expr2 = -2* Expr::from("a") - 4 * Expr::from("c") + 1;
+    let expr1 = -2* Expr::var("a") + 3 *  Expr::var("b") - 4 * Expr::var("c") + 1;
+    let expr2 = -2* Expr::var("a") - 4 * Expr::var("c") + 1;
 
-    let constraint1 = expr1.leq(&0.into());
-    let constraint2 = expr2.eq(&0.into());
-    let constraint3 = expr2.geq(&(-2).into());
+    let constraint1 = expr1.leq(&Expr::constant(0));
+    let constraint2 = expr2.eq(&Expr::constant(0));
+    let constraint3 = expr2.geq(&Expr::constant(-2));
 
     let config = Config {
         values: BTreeMap::from([
