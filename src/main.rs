6fa102ff021a9aa04364d3a04c2b6c1f70648b10
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:33:27 +0200

    Stub sqlx code in main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use async_std::task;
+use anyhow::anyhow;
+use anyhow::Result;
 use clap::Parser;
-
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 #[derive(Parser)]
 #[command(version, about)]
@@ -10,17 +9,47 @@ struct Args {
     db: std::path::PathBuf,
 }
 
-async fn test_db() -> Result<()> {
+use sqlx::migrate::MigrateDatabase;
+use sqlx::sqlite::SqliteConnection;
+use sqlx::Connection;
+
+async fn create_db(db_url: &str) -> Result<()> {
+    sqlx::Sqlite::create_database(db_url).await?;
+    let mut db = SqliteConnection::connect(&db_url).await?;
+
+    sqlx::query("CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY NOT NULL, surname STRING NOT NULL, firstname STRING NOT NULL);")
+        .execute(&mut db).await?;
+
     Ok(())
 }
 
-fn main() -> Result<()> {
+async fn open_db(path: &std::path::Path) -> Result<SqliteConnection> {
+    let filename = match path.to_str() {
+        Some(f) => f,
+        None => return Err(anyhow!("Non UTF-8 file name")),
+    };
+    let db_url = format!("sqlite://{}", filename);
+
+    if !sqlx::Sqlite::database_exists(&db_url)
+        .await
+        .unwrap_or(false)
+    {
+        create_db(&db_url).await?;
+    }
+
+    Ok(SqliteConnection::connect(&db_url).await?)
+}
+
+#[async_std::main]
+async fn main() -> Result<()> {
     let args = Args::parse();
 
-    println!("{}", args.db.to_string_lossy());
+    let mut db = open_db(args.db.as_path()).await?;
+    let result = sqlx::query("SELECT * FROM students;")
+        .execute(&mut db)
+        .await?;
 
-    let fut = test_db();
-    task::block_on(fut)?;
+    println!("{:?}", result);
 
     Ok(())
 }
