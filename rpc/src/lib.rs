df5cdab6056bb5221be5e3329f5e08858a48f8ab
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:16 +0200

    Add structure code to send commands

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -3,20 +3,61 @@
 //! This crate contains the code to run an rpc server
 //! as well as the necessary RCP messages
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub enum InitMsg {
     Greetings,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub enum OutMsg {
     Ack,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 pub enum CmdMsg {
     Success,
     Warning,
+}
+
+impl InitMsg {
+    pub fn from_text_msg(data: &str) -> Result<Self, String> {
+        match serde_json::from_str::<Self>(data) {
+            Ok(cmd) => Ok(cmd),
+            Err(_) => Err(data.to_string()),
+        }
+    }
+
+    pub fn into_text_msg(&self) -> String {
+        serde_json::to_string(self).expect("Serializing to JSON should not fail")
+    }
+}
+
+impl OutMsg {
+    pub fn from_text_msg(data: &str) -> Result<Self, String> {
+        match serde_json::from_str::<Self>(data) {
+            Ok(cmd) => Ok(cmd),
+            Err(_) => Err(data.to_string()),
+        }
+    }
+
+    pub fn into_text_msg(&self) -> String {
+        serde_json::to_string(self).expect("Serializing to JSON should not fail")
+    }
+}
+
+impl CmdMsg {
+    pub fn from_text_msg(data: &str) -> Result<Self, String> {
+        match serde_json::from_str::<Self>(data) {
+            Ok(cmd) => Ok(cmd),
+            Err(_) => Err(data.to_string()),
+        }
+    }
+
+    pub fn into_text_msg(&self) -> String {
+        serde_json::to_string(self).expect("Serializing to JSON should not fail")
+    }
 }
 
 /// Main RPC Engine function
