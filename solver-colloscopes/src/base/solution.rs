df286a470331c355f73688d5452b2f4d2e99c71e
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:15 +0200

    Add submodule structure

---
+++ solver-colloscopes/src/base/solution.rs
@@ -0,0 +1,62 @@
+//! Solution submodule of [crate::base].
+//! 
+//! This submodule defines the various types to describe a colloscope.
+//! 
+//! The main such structure is [Colloscope] which describes
+//! a (partially completed or not) colloscope.
+use std::collections::{BTreeMap, BTreeSet};
+use std::num::NonZeroUsize;
+
+pub trait Identifier : Clone + Copy + std::fmt::Debug + Ord + PartialOrd + Eq + PartialEq + Send + Sync {}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct GroupList<StudentId: Identifier> {
+    group_count: usize,
+    assigned_students: BTreeMap<StudentId, usize>,
+    unassigned_students: BTreeSet<StudentId>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Interrogation {
+    assigned_groups: BTreeSet<usize>,
+    unassigned_groups: BTreeSet<usize>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct InterrogationSlot {
+    slot_start: collomatique_time::SlotStart,
+    interrogations: Vec<Option<Interrogation>>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct TeacherInterrogations<InterrogationId: Identifier> {
+    interrogation_slots: BTreeMap<InterrogationId, InterrogationSlot>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct GroupAssignment<GroupListId: Identifier, StudentId: Identifier> {
+    group_list_id: GroupListId,
+    enrolled_students: BTreeSet<StudentId>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct DatedGroupAssignment<GroupListId: Identifier, StudentId: Identifier> {
+    start_week: NonZeroUsize,
+    group_assignment: GroupAssignment<GroupListId, StudentId>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct SubjectInterrogations<TeacherId: Identifier, InterrogationId: Identifier, GroupListId: Identifier, StudentId: Identifier> {
+    duration: collomatique_time::NonZeroDurationInMinutes,
+    starting_group_assignment: GroupAssignment<GroupListId, StudentId>,
+    other_group_assignments: Vec<DatedGroupAssignment<GroupListId, StudentId>>,
+    teacher_map: BTreeMap<TeacherId, TeacherInterrogations<InterrogationId>>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Colloscope<SubjectId: Identifier, TeacherId: Identifier, InterrogationId: Identifier, GroupListId: Identifier, StudentId: Identifier> {
+    week_count: usize,
+    subject_map: BTreeMap<SubjectId, SubjectInterrogations<TeacherId, InterrogationId, GroupListId, StudentId>>,
+    group_lists: BTreeMap<GroupListId, GroupList<StudentId>>,
+}
+
