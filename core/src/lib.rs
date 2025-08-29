bece39c6acd1d3e798cbcde81bb5f2a3cb39d653
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:15 +0200

    Add core and gtk4 stub

---
+++ core/src/lib.rs
@@ -0,0 +1,5 @@
+//! Core functionnality of collomatique
+//!
+//! This crate defines the core functionnality of collomatique in a
+//! UI-agnostic way. This should allow for implementation of different
+//! UIs all using the same core code.
