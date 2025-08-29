7a7cfbecce1eba78606da4619f2410f8f4248cd5
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:13 +0200

    Add group lists to colloscope constraint description in collomatique-core

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -42,9 +42,22 @@ pub struct Student {
     pub non_consecutive_interrogations: bool,
 }
 
+/// Description of the constraints for a group list
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct GroupList {
+    /// Range of allowed number of students per group
+    pub students_per_group: RangeInclusive<NonZeroUsize>,
+    /// Students that should be included in the list
+    pub students: BTreeSet<usize>,
+    /// Range of allowed number of groups in the list
+    pub group_count: RangeInclusive<usize>,
+}
+
 /// Description of all the data describing a colloscope constraints
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct ColloscopeConstraints {
     /// List of students with their relevant constraints
     pub students: Vec<Student>,
+    /// List of groups constraints
+    pub group_lists: Vec<GroupList>,
 }
