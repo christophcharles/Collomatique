089a40e0c6e8849ca11c7a82c66621558072bb41
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:14 +0200

    Add stub structs for colloscopes

--- core/src/colloscopes.rs
+++ core/src/colloscopes.rs
@@ -3,3 +3,7 @@
 //! This modules contain the code that defines the various colloscopes constraints.
 //! It builds on [crate::generics] and [crate::solver] and uses its interfaces
 //! to build a *nice-enough* interface for automatic solving of colloscopes.
+//! 
+
+pub mod structs;
+
