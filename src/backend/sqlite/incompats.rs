278482df1e58854beab0d27fb0eb79ecdb19ef55
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:33:31 +0200

    Add incompats_get_all

--- src/backend/sqlite/incompats.rs
+++ src/backend/sqlite/incompats.rs
@@ -107,8 +107,115 @@ WHERE incompat_id = ?
     })
 }
 
-pub async fn get_all(_pool: &SqlitePool) -> Result<BTreeMap<Id, Incompat<week_patterns::Id>>> {
-    todo!()
+pub async fn get_all(pool: &SqlitePool) -> Result<BTreeMap<Id, Incompat<week_patterns::Id>>> {
+    let incompats_db = sqlx::query!("SELECT incompat_id, name, max_count FROM incompats")
+        .fetch_all(pool)
+        .await
+        .map_err(Error::from)?;
+
+    let mut incompats = incompats_db
+        .into_iter()
+        .map(|x| {
+            let max_count_usize = usize::try_from(x.max_count).map_err(|_| {
+                Error::CorruptedDatabase(format!(
+                    "Database has invalid max_count ({}) for incompat_id {}",
+                    x.max_count, x.incompat_id
+                ))
+            })?;
+
+            Result::Ok((
+                Id(x.incompat_id),
+                Incompat {
+                    name: x.name,
+                    max_count: max_count_usize,
+                    groups: BTreeSet::<IncompatGroup<week_patterns::Id>>::new(),
+                },
+            ))
+        })
+        .collect::<Result<BTreeMap<_, _>>>()?;
+
+    for (id, incompat) in incompats.iter_mut() {
+        let incompat_id = id.0;
+
+        let data = sqlx::query!(
+            r#"
+SELECT incompat_group_items.incompat_group_id AS incompat_group_id, week_pattern_id, start_day, start_time, duration
+FROM incompat_groups JOIN incompat_group_items
+ON incompat_groups.incompat_group_id = incompat_group_items.incompat_group_id
+WHERE incompat_id = ?
+            "#,
+            incompat_id
+        )
+        .fetch_all(pool)
+        .await
+        .map_err(Error::from)?;
+
+        let mut temp_groups: BTreeMap<i64, IncompatGroup<week_patterns::Id>> = BTreeMap::new();
+
+        for x in data {
+            let week_pattern_id = week_patterns::Id(x.week_pattern_id);
+
+            let start_day_usize = usize::try_from(x.start_day).map_err(|_| {
+                Error::CorruptedDatabase(format!(
+                    "Database references invalid start day ({}) for incompat_group_id {}",
+                    x.start_day, x.incompat_group_id
+                ))
+            })?;
+            let day = crate::time::Weekday::try_from(start_day_usize).map_err(|e| {
+                Error::CorruptedDatabase(format!(
+                    "Database references invalid start day ({}) for incompat_group_id {}: {}",
+                    start_day_usize, x.incompat_group_id, e
+                ))
+            })?;
+            let start_time_u32 = u32::try_from(x.start_time).map_err(|_| {
+                Error::CorruptedDatabase(format!(
+                    "Database references invalid start time ({}) for incompat_group_id {}",
+                    x.start_time, x.incompat_group_id
+                ))
+            })?;
+            let time =
+                crate::time::Time::new(start_time_u32).ok_or(Error::CorruptedDatabase(format!(
+                    "Database references invalid start time ({}) for incompat_group_id {}",
+                    start_time_u32, x.incompat_group_id
+                )))?;
+            let start = TimeSlot { day, time };
+
+            let duration_u32 = u32::try_from(x.duration).map_err(|_| {
+                Error::CorruptedDatabase(format!(
+                    "Database references invalid duration ({}) for incompat_group_id {}",
+                    x.duration, x.incompat_group_id
+                ))
+            })?;
+            let duration =
+                NonZeroU32::new(duration_u32).ok_or(Error::CorruptedDatabase(format!(
+                    "Database references invalid duration ({}) for incompat_group_id {}",
+                    duration_u32, x.incompat_group_id
+                )))?;
+
+            let slot = IncompatSlot {
+                week_pattern_id,
+                start,
+                duration,
+            };
+            match temp_groups.get_mut(&x.incompat_group_id) {
+                Some(group) => {
+                    group.slots.insert(slot);
+                }
+                None => {
+                    temp_groups.insert(
+                        x.incompat_group_id,
+                        IncompatGroup {
+                            slots: BTreeSet::from([slot]),
+                        },
+                    );
+                }
+            }
+        }
+
+        incompat.groups = temp_groups.into_iter().map(|(_key, group)| group).collect();
+    }
+
+    Ok(incompats)
 }
 
 pub async fn add(
