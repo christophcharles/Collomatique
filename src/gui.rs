0c12d905b8162b639fa0c51f724fe5bd94ea720d
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:10 +0200

    Add open and create dialogs

--- src/gui.rs
+++ src/gui.rs
@@ -1,27 +1,40 @@
 use anyhow::Result;
 
+mod dialogs;
 mod editor;
+mod tools;
 mod welcome;
 
-use iced::Element;
+use iced::{Element, Task};
 
-#[derive(Default)]
+#[derive(Default, Debug, Clone)]
 enum GuiState {
     #[default]
     Welcome,
     Editor(editor::State),
+    DialogShown(dialogs::State),
 }
 
 #[derive(Debug, Clone)]
 enum GuiMessage {
     WelcomeMessage(welcome::Message),
     EditorMessage(editor::Message),
+    DialogMessage(dialogs::Message),
+    FileSelected(Option<dialogs::FileDesc>),
 }
 
-fn update(state: &mut GuiState, message: GuiMessage) {
+fn update(state: &mut GuiState, message: GuiMessage) -> Task<GuiMessage> {
     match message {
         GuiMessage::EditorMessage(msg) => editor::update(state, msg),
         GuiMessage::WelcomeMessage(msg) => welcome::update(state, msg),
+        GuiMessage::DialogMessage(msg) => dialogs::update(state, msg),
+        GuiMessage::FileSelected(result) => match result {
+            Some(file_desc) => {
+                *state = GuiState::Editor(editor::State::new(file_desc.path));
+                Task::none()
+            }
+            None => Task::none(),
+        },
     }
 }
 
@@ -29,6 +42,7 @@ fn view(state: &GuiState) -> Element<GuiMessage> {
     match state {
         GuiState::Welcome => welcome::view(),
         GuiState::Editor(editor_state) => editor::view(editor_state),
+        GuiState::DialogShown(dialog_state) => dialogs::view(dialog_state),
     }
 }
 
@@ -36,23 +50,22 @@ fn title(state: &GuiState) -> String {
     match state {
         GuiState::Welcome => welcome::title(),
         GuiState::Editor(editor_state) => editor::title(editor_state),
+        GuiState::DialogShown(dialog_state) => dialogs::title(dialog_state),
     }
 }
 
 pub fn run_gui(create: bool, db: Option<std::path::PathBuf>) -> Result<()> {
     iced::application(title, update, view).run_with(move || {
         (
-            match db {
-                Some(file) => GuiState::Editor(editor::State::new(file)),
-                None => {
-                    if create {
-                        GuiState::Editor(editor::State::default())
-                    } else {
-                        GuiState::Welcome
-                    }
-                }
+            match &db {
+                Some(file) => GuiState::Editor(editor::State::new(file.clone())),
+                None => GuiState::Welcome,
             },
-            iced::Task::none(),
+            if create && db.is_none() {
+                iced::Task::done(GuiMessage::DialogMessage(dialogs::Message::OpenNewFile))
+            } else {
+                iced::Task::none()
+            },
         )
     })?;
     Ok(())
---
+++ src/gui/tools.rs
@@ -0,0 +1,27 @@
+use iced::widget::{center, container, opaque, stack};
+use iced::{Color, Element};
+
+pub fn modal<'a, Message>(
+    base: impl Into<Element<'a, Message>>,
+    content: impl Into<Element<'a, Message>>,
+) -> Element<'a, Message>
+where
+    Message: Clone + 'a,
+{
+    stack![
+        base.into(),
+        opaque(center(content).style(|_theme| {
+            container::Style {
+                background: Some(
+                    Color {
+                        a: 0.8,
+                        ..Color::BLACK
+                    }
+                    .into(),
+                ),
+                ..container::Style::default()
+            }
+        }))
+    ]
+    .into()
+}
