3a36393e0ed0f231c657211994c320e4d48ad8b8
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:33:27 +0200

    Add Knuth inspired heuristic for backtracking and update tests accordingly

--- tests/backtracking.rs
+++ tests/backtracking.rs
@@ -311,12 +311,20 @@ fn restricted_interrogations_per_week() {
     let ilp_translator = data.ilp_translator();
     let problem = ilp_translator.problem();
 
-    use collomatique::ilp::solvers::backtracking::heuristics::Connolly1992;
-    let solver = collomatique::ilp::solvers::backtracking::Solver::new(Connolly1992::default());
+    use collomatique::ilp::solvers::backtracking::heuristics::Knuth2000;
+    let solver = collomatique::ilp::solvers::backtracking::Solver::new(Knuth2000::default());
 
     use collomatique::gen::colloscope::Variable;
     // Through testing this one caused issue when trying to put GiS_2_0_2 to 1
-    /* GiS_0_0_0: 0, GiS_0_0_1: 0, GiS_0_0_2: 1, GiS_0_0_3: 0, GiS_0_1_0: 0, GiS_0_1_1: 1, GiS_0_1_2: 0, GiS_0_1_3: 0, GiS_0_2_0: 0, GiS_0_2_1: 0, GiS_0_2_2: 0, GiS_0_2_3: 1, GiS_0_3_0: 1, GiS_0_3_1: 0, GiS_0_3_2: 0, GiS_0_3_3: 0, GiS_0_4_0: 0, GiS_0_4_1: 0, GiS_0_4_2: 1, GiS_0_4_3: 0, GiS_0_5_0: 0, GiS_0_5_1: 1, GiS_0_5_2: 0, GiS_0_5_3: 0, GiS_0_6_0: 1, GiS_0_6_1: 0, GiS_0_6_2: 0, GiS_0_6_3: 0, GiS_0_7_0: 0, GiS_0_7_1: 0, GiS_0_7_2: 0, GiS_0_7_3: 1, GiS_1_0_0: 0, GiS_1_0_1: 0, GiS_1_0_2: 0, GiS_1_0_3: 1, GiS_1_1_0: 0, GiS_1_1_1: 0, GiS_1_1_2: 1, GiS_1_1_3: 0, GiS_1_2_0: 1, GiS_1_2_1: 0, GiS_1_2_2: 0, GiS_1_2_3: 0, GiS_1_3_0: 0, GiS_1_3_1: 1, GiS_1_3_2: 0, GiS_1_3_3: 0, GiS_2_0_0: 1, GiS_2_0_1: 0, GiS_2_0_2: 0, GiS_2_1_0: 0, GiS_2_1_1: 1, GiS_2_1_2: 0, GiS_2_2_0: 0, GiS_2_2_1: 0, GiS_2_2_2: 0, GiS_2_3_0: 0, GiS_2_3_1: 0, GiS_2_3_2: 1, GiS_3_0_0: 0, GiS_3_1_0: 1 */
+    /* GiS_0_0_0: 0, GiS_0_0_1: 0, GiS_0_0_2: 1, GiS_0_0_3: 0, GiS_0_1_0: 0, GiS_0_1_1: 1, GiS_0_1_2: 0,
+    GiS_0_1_3: 0, GiS_0_2_0: 0, GiS_0_2_1: 0, GiS_0_2_2: 0, GiS_0_2_3: 1, GiS_0_3_0: 1, GiS_0_3_1: 0,
+    GiS_0_3_2: 0, GiS_0_3_3: 0, GiS_0_4_0: 0, GiS_0_4_1: 0, GiS_0_4_2: 1, GiS_0_4_3: 0, GiS_0_5_0: 0,
+    GiS_0_5_1: 1, GiS_0_5_2: 0, GiS_0_5_3: 0, GiS_0_6_0: 1, GiS_0_6_1: 0, GiS_0_6_2: 0, GiS_0_6_3: 0,
+    GiS_0_7_0: 0, GiS_0_7_1: 0, GiS_0_7_2: 0, GiS_0_7_3: 1, GiS_1_0_0: 0, GiS_1_0_1: 0, GiS_1_0_2: 0,
+    GiS_1_0_3: 1, GiS_1_1_0: 0, GiS_1_1_1: 0, GiS_1_1_2: 1, GiS_1_1_3: 0, GiS_1_2_0: 1, GiS_1_2_1: 0,
+    GiS_1_2_2: 0, GiS_1_2_3: 0, GiS_1_3_0: 0, GiS_1_3_1: 1, GiS_1_3_2: 0, GiS_1_3_3: 0, GiS_2_0_0: 1,
+    GiS_2_0_1: 0, GiS_2_0_2: 0, GiS_2_1_0: 0, GiS_2_1_1: 1, GiS_2_1_2: 0, GiS_2_2_0: 0, GiS_2_2_1: 0,
+    GiS_2_2_2: 0, GiS_2_3_0: 0, GiS_2_3_1: 0, GiS_2_3_2: 1, GiS_3_0_0: 0, GiS_3_1_0: 1 */
     let config = problem
         .config_from([
             &Variable::GroupInSlot {
@@ -415,16 +423,107 @@ fn restricted_interrogations_per_week() {
         )
         .unwrap();
 
+    // Knuth heuristic should give this :
+    /* GiS_0_0_0: 0, GiS_0_0_1: 0, GiS_0_0_2: 1, GiS_0_0_3: 0, GiS_0_1_0: 0, GiS_0_1_1: 1, GiS_0_1_2: 0,
+    GiS_0_1_3: 0, GiS_0_2_0: 0, GiS_0_2_1: 0, GiS_0_2_2: 0, GiS_0_2_3: 1, GiS_0_3_0: 1, GiS_0_3_1: 0,
+    GiS_0_3_2: 0, GiS_0_3_3: 0, GiS_0_4_0: 0, GiS_0_4_1: 0, GiS_0_4_2: 1, GiS_0_4_3: 0, GiS_0_5_0: 0,
+    GiS_0_5_1: 1, GiS_0_5_2: 0, GiS_0_5_3: 0, GiS_0_6_0: 1, GiS_0_6_1: 0, GiS_0_6_2: 0, GiS_0_6_3: 0,
+    GiS_0_7_0: 0, GiS_0_7_1: 0, GiS_0_7_2: 0, GiS_0_7_3: 1, GiS_1_0_0: 1, GiS_1_0_1: 0, GiS_1_0_2: 0,
+    GiS_1_0_3: 0, GiS_1_1_0: 0, GiS_1_1_1: 1, GiS_1_1_2: 0, GiS_1_1_3: 0, GiS_1_2_0: 0, GiS_1_2_1: 0,
+    GiS_1_2_2: 0, GiS_1_2_3: 1, GiS_1_3_0: 0, GiS_1_3_1: 0, GiS_1_3_2: 1, GiS_1_3_3: 0, GiS_2_0_0: 0,
+    GiS_2_0_1: 0, GiS_2_0_2: 1, GiS_2_1_0: 0, GiS_2_1_1: 0, GiS_2_1_2: 0, GiS_2_2_0: 0, GiS_2_2_1: 1,
+    GiS_2_2_2: 0, GiS_2_3_0: 1, GiS_2_3_1: 0, GiS_2_3_2: 0, GiS_3_0_0: 1, GiS_3_1_0: 0 */
+    // I'm not quite sure to what extent this is dependent on the precise implementation but anyway,
+    // this test should avoid regression
+    let config3 = problem
+        .config_from([
+            &Variable::GroupInSlot {
+                subject: 0,
+                slot: 0,
+                group: 2,
+            },
+            &Variable::GroupInSlot {
+                subject: 0,
+                slot: 1,
+                group: 1,
+            },
+            &Variable::GroupInSlot {
+                subject: 0,
+                slot: 2,
+                group: 3,
+            },
+            &Variable::GroupInSlot {
+                subject: 0,
+                slot: 3,
+                group: 0,
+            },
+            &Variable::GroupInSlot {
+                subject: 0,
+                slot: 4,
+                group: 2,
+            },
+            &Variable::GroupInSlot {
+                subject: 0,
+                slot: 5,
+                group: 1,
+            },
+            &Variable::GroupInSlot {
+                subject: 0,
+                slot: 6,
+                group: 0,
+            },
+            &Variable::GroupInSlot {
+                subject: 0,
+                slot: 7,
+                group: 3,
+            },
+            &Variable::GroupInSlot {
+                subject: 1,
+                slot: 0,
+                group: 0,
+            },
+            &Variable::GroupInSlot {
+                subject: 1,
+                slot: 1,
+                group: 1,
+            },
+            &Variable::GroupInSlot {
+                subject: 1,
+                slot: 2,
+                group: 3,
+            },
+            &Variable::GroupInSlot {
+                subject: 1,
+                slot: 3,
+                group: 2,
+            },
+            &Variable::GroupInSlot {
+                subject: 2,
+                slot: 0,
+                group: 2,
+            },
+            &Variable::GroupInSlot {
+                subject: 2,
+                slot: 2,
+                group: 1,
+            },
+            &Variable::GroupInSlot {
+                subject: 2,
+                slot: 3,
+                group: 0,
+            },
+            &Variable::GroupInSlot {
+                subject: 3,
+                slot: 0,
+                group: 0,
+            },
+        ])
+        .unwrap();
+    let expected_result = config3.into_feasable().unwrap();
+
     use collomatique::ilp::solvers::FeasabilitySolver;
 
-    let result = solver.restore_feasability_with_origin_and_max_steps(
-        &config2,
-        Some(&origin_config),
-        Some(5000),
-    );
+    let result = solver.restore_feasability_with_origin(&config2, Some(&origin_config));
 
-    match result {
-        None => panic!("None"),
-        Some(c) => panic!("{}", c),
-    };
+    assert_eq!(result.expect("Should find a solution"), expected_result);
 }
