270a3ebbbad6a6df317b83449cf3d82d417cc034
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:10 +0200

    Start mock up of colloscope interface

--- src/gui.rs
+++ src/gui.rs
@@ -1,39 +1,105 @@
 use anyhow::Result;
 
-use iced::widget::{button, column, container, text};
+use iced::widget::{button, column, container, row, text};
 use iced::Element;
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Panel {
+    SubjectGroups,
+    Subjects,
+    Teachers,
+    Students,
+}
+
 struct GuiState {
-    txt: String,
+    panel: Panel,
 }
 
 impl Default for GuiState {
     fn default() -> Self {
         Self {
-            txt: String::from("Placeholder"),
+            panel: Panel::SubjectGroups,
         }
     }
 }
 
 #[derive(Debug, Clone)]
-enum Message {}
+enum Message {
+    PanelChanged(Panel),
+}
 
-fn update(_state: &mut GuiState, _message: Message) {}
+fn update(state: &mut GuiState, message: Message) {
+    match message {
+        Message::PanelChanged(new_panel) => {
+            state.panel = new_panel;
+        }
+    }
+}
 
 fn view(state: &GuiState) -> Element<Message> {
     use iced::Length;
 
-    column![
-        container(button("Ouvrir"))
-            .padding(5)
-            .width(Length::Fill)
-            .center_y(Length::Shrink)
-            .style(iced::widget::container::rounded_box),
-        container(text(&state.txt))
+    container(
+        row![
+            column![
+                container(
+                    column![
+                        button("Groupements")
+                            .width(Length::Fill)
+                            .style(if state.panel == Panel::SubjectGroups {
+                                button::primary
+                            } else {
+                                button::text
+                            })
+                            .on_press(Message::PanelChanged(Panel::SubjectGroups)),
+                        button("Matières")
+                            .width(Length::Fill)
+                            .style(if state.panel == Panel::Subjects {
+                                button::primary
+                            } else {
+                                button::text
+                            })
+                            .on_press(Message::PanelChanged(Panel::Subjects)),
+                        button("Enseignants")
+                            .width(Length::Fill)
+                            .style(if state.panel == Panel::Teachers {
+                                button::primary
+                            } else {
+                                button::text
+                            })
+                            .on_press(Message::PanelChanged(Panel::Teachers)),
+                        button("Élèves")
+                            .width(Length::Fill)
+                            .style(if state.panel == Panel::Students {
+                                button::primary
+                            } else {
+                                button::text
+                            })
+                            .on_press(Message::PanelChanged(Panel::Students)),
+                    ]
+                    .width(Length::Fill)
+                    .spacing(2)
+                )
+                .padding(5)
+                .height(Length::Fill)
+                .center_x(Length::Shrink)
+                .style(iced::widget::container::rounded_box),
+                button(container(text("Menu")).center_x(Length::Fill)).width(Length::Fill),
+            ]
+            .width(200)
+            .spacing(2),
+            container(text(match state.panel {
+                Panel::SubjectGroups => "Panneau groupements",
+                Panel::Subjects => "Panneau matières",
+                Panel::Teachers => "Panneau enseignants",
+                Panel::Students => "Panneau élèves",
+            }))
             .center_x(Length::Fill)
             .center_y(Length::Fill)
-    ]
-    .spacing(5)
+        ]
+        .spacing(5),
+    )
+    .padding(5)
     .into()
 }
 
