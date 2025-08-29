7d5c844f0b75675f4c4de8be24a70beff294fd71
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:13 +0200

    Add full example in doc

--- collomatique-ilp/src/lib.rs
+++ collomatique-ilp/src/lib.rs
@@ -49,7 +49,111 @@
 //! - to have a possibility to simply check if a possible solution is indeed a solution without calling a solver. And in case it is not
 //!   a solution to be able to trace which constraints are not satisfied.
 //!
-//! The normal workflow with this crate is to start with a [ProblemBuilder].
+//! The normal workflow with this crate is to start with a [ProblemBuilder]. Once the [Problem] is build
+//! we can solve it using a [solvers::Solver].
+//! Alternatively, we can build a configuration using [ConfigData] and [Config] and use [Config::blame] to
+//! find out which constraints are not satisfied.
+//! 
+//! Let's now discuss a somewhat complete example.
+//! 
+//! Example
+//! ---
+//! 
+//! We have two student groups X and Y.
+//! They must both attend exactly once two different courses (1 and 2)
+//! on the span of two weeks.
+//! But the courses happen simultaneously.
+//!
+//! This means we must fill a timetable of the following form:
+//! |          | Week 1 | Week 2 |
+//! |----------|--------|--------|
+//! | Course 1 |        |        |
+//! | Course 2 |        |        |
+//!
+//! by putting an x or a y in each cell.
+//!
+//! We represent this with 8 boolean variables.
+//! The variable `xij` is 1 if X is written in the cell on the line i and column j, 0 otherwise.
+//! The same pattern is used for `yij`.
+//! 
+//! We have three broad conditions :
+//! - We should not put an X and a Y in the same cell. But a cell can possibly be empty.
+//!   This means that for all i and j, we have `xij + yij <= 1`.
+//! - We should not put two Xs or two Ys in the same column (but column could have zero).
+//!   This means that for all j, we should have `x1j + x2j <= 1` and the same for the `yij`.
+//! - We must put exactly one X and one Y on each line.
+//!   This means that `xi1 + xi2 = 1` for all i and the same for the group Y.
+//!
+//! Additionally, we will prefer having group X in course 1 on week 1 by using an objective function.
+//! We just use the expression `x11` which is indeed linear. If we maximize it, this should
+//! value having gorup X in course 1 on week 1 rather than not.
+//!
+//! ```
+//! # use collomatique_ilp::{ProblemBuilder, LinExpr, Variable, ObjectiveSense};
+//! let x11 = LinExpr::<String>::var("x11"); // Group X has course 1 on week 1
+//! let x12 = LinExpr::<String>::var("x12"); // Group X has course 1 on week 2
+//! let x21 = LinExpr::<String>::var("x21"); // Group X has course 2 on week 1
+//! let x22 = LinExpr::<String>::var("x22"); // Group X has course 2 on week 2
+//!
+//! let y11 = LinExpr::<String>::var("y11"); // Group Y has course 1 on week 1
+//! let y12 = LinExpr::<String>::var("y12"); // Group Y has course 1 on week 2
+//! let y21 = LinExpr::<String>::var("y21"); // Group Y has course 2 on week 1
+//! let y22 = LinExpr::<String>::var("y22"); // Group Y has course 2 on week 2
+//!
+//! let one = LinExpr::<String>::constant(1.0); // Constant for easier writing of constraints
+//!
+//! let pb = ProblemBuilder::<String, String>::new()
+//!     .set_variables([
+//!         ("x11", Variable::binary()),
+//!         ("x12", Variable::binary()),
+//!         ("x21", Variable::binary()),
+//!         ("x22", Variable::binary())
+//!     ])
+//!     .set_variables([
+//!         ("y11", Variable::binary()),
+//!         ("y12", Variable::binary()),
+//!         ("y21", Variable::binary()),
+//!         ("y22", Variable::binary())
+//!     ])
+//!     // Both class should not attend a course at the same time
+//!     .add_constraints([
+//!         ((&x11 + &y11).leq(&one), "At most one group in course 1 on week 1"),
+//!         ((&x12 + &y12).leq(&one), "At most one group in course 1 on week 2"),
+//!         ((&x21 + &y21).leq(&one), "At most one group in course 2 on week 1"),
+//!         ((&x22 + &y22).leq(&one), "At most one group in course 2 on week 2")
+//!     ])
+//!     // Each class should not attend more than one course at a given time
+//!     .add_constraints([
+//!         ((&x11 + &x21).leq(&one), "At most one course for group X on week 1"),
+//!         ((&x12 + &x22).leq(&one), "At most one course for group X on week 2"),
+//!         ((&y11 + &y21).leq(&one), "At most one course for group Y on week 1"),
+//!         ((&y12 + &y22).leq(&one), "At most one course for group Y on week 2")
+//!     ])
+//!     // Each class must complete each course exactly once
+//!     .add_constraints([
+//!         ((&x11 + &x12).eq(&one), "Group X should have course 1 exactly once"),
+//!         ((&x21 + &x22).eq(&one), "Group X should have course 2 exactly once"),
+//!         ((&y11 + &y12).eq(&one), "Group Y should have course 1 exactly once"),
+//!         ((&y21 + &y22).eq(&one), "Group Y should have course 2 exactly once")
+//!     ])
+//!     // Objective function : prefer group X in course 1 on week 1
+//!     .set_objective_function(x11.clone(), ObjectiveSense::Maximize)
+//!     .build()
+//!     .unwrap();
+//! 
+//! // Now pb represents our ILP problem. We can solve it using a solver
+//! # use collomatique_ilp::solvers;
+//! let solver = solvers::coin_cbc::CbcSolver::new();
+//! 
+//! use solvers::Solver;
+//! // Solver::solve returns None when there is no solution
+//! let config = solver.solve(&pb).expect("There should be a solution");
+//! 
+//! // Now config is the optimal solution. We can prob it/
+//! // Because Group X should have course 1 on week 1 (this is prefered by the objective function)
+//! // Group Y should have course 2 the same week. Let's check that:
+//! assert_eq!(config.get("y21"), Some(1.0));
+//! ```
 
 pub mod linexpr;
 pub mod mat_repr;
