13b832a178aa4d49e6eb6529f243bddd2352a1f9
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:20 +0200

    Add solver-groups crate

---
+++ solver-groups/src/base.rs
@@ -0,0 +1,31 @@
+//! Definition of relevant structures to describe group lists
+
+pub mod solution;
+pub mod variables;
+
+use std::collections::{BTreeMap, BTreeSet};
+use std::num::NonZeroU32;
+use std::ops::RangeInclusive;
+
+pub trait Identifier:
+    Clone + Copy + std::fmt::Debug + Ord + PartialOrd + Eq + PartialEq + Send + Sync
+{
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SubjectDescription<
+    StudentId: Identifier,
+> {
+    students: BTreeSet<StudentId>,
+    group_count: RangeInclusive<u32>,
+    students_per_group: RangeInclusive<NonZeroU32>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct GroupListProblem<
+    SubjectId: Identifier,
+    StudentId: Identifier,
+> {
+    subject_descriptions:
+        BTreeMap<SubjectId, SubjectDescription<StudentId>>,
+}
---
+++ solver-groups/src/lib.rs
@@ -0,0 +1,7 @@
+//! Collomatique-solver-groups
+//! ---
+//!
+//! This crate contains a simplified constraint system to automatically create group lists.
+
+pub mod base;
+pub mod constraints;
