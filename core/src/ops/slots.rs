8fcdbe122bf220ff1a9d1355020e7c697c52e68a
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:20 +0200

    New stub structure for iterative cleaning and warnings

--- core/src/ops/slots.rs
+++ core/src/ops/slots.rs
@@ -119,12 +119,21 @@ pub enum MoveSlotDownError {
 }
 
 impl SlotsUpdateOp {
-    pub(crate) fn get_cleaning_ops<
+    pub(crate) fn get_next_cleaning_op<
         T: collomatique_state::traits::Manager<Data = Data, Desc = Desc>,
     >(
         &self,
-        data: &T,
-    ) -> CleaningOps<SlotsUpdateWarning> {
+        _data: &T,
+    ) -> Option<CleaningOp<SlotsUpdateWarning>> {
+        todo!()
+    }
+
+    pub(crate) fn apply_no_cleaning<
+        T: collomatique_state::traits::Manager<Data = Data, Desc = Desc>,
+    >(
+        &self,
+        data: &mut T,
+    ) -> Result<Option<collomatique_state_colloscopes::SlotId>, SlotsUpdateError> {
         todo!()
     }
 
--- core/src/ops/students.rs
+++ core/src/ops/students.rs
@@ -122,12 +122,21 @@ pub enum DeleteStudentError {
 }
 
 impl StudentsUpdateOp {
-    pub(crate) fn get_cleaning_ops<
+    pub(crate) fn get_next_cleaning_op<
         T: collomatique_state::traits::Manager<Data = Data, Desc = Desc>,
     >(
         &self,
-        data: &T,
-    ) -> CleaningOps<StudentsUpdateWarning> {
+        _data: &T,
+    ) -> Option<CleaningOp<StudentsUpdateWarning>> {
+        todo!()
+    }
+
+    pub(crate) fn apply_no_cleaning<
+        T: collomatique_state::traits::Manager<Data = Data, Desc = Desc>,
+    >(
+        &self,
+        data: &mut T,
+    ) -> Result<Option<collomatique_state_colloscopes::StudentId>, StudentsUpdateError> {
         todo!()
     }
 
