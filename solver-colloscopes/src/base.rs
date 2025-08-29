1fef1a6bca74e7e6c2baf441bb8e246290c482c9
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:15 +0200

    Update structs for colloscopes

--- solver-colloscopes/src/base.rs
+++ solver-colloscopes/src/base.rs
@@ -4,33 +4,46 @@
 //! The main such structure is [Colloscope] which describes
 //! a (partially completed or not) colloscope.
 
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, BTreeSet};
 use std::num::NonZeroUsize;
 
 pub trait Identifier : Clone + Copy + std::fmt::Debug + Ord + PartialOrd + Eq + PartialEq + std::hash::Hash + Send + Sync {}
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct GroupList<StudentId: Identifier> {
-    students_per_group: std::ops::RangeInclusive<usize>,
-    groups: Vec<HashSet<StudentId>>,
+    group_count: usize,
+    assigned_students: HashMap<StudentId, usize>,
+    unassigned_students: HashSet<StudentId>,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct TeacherInterrogations {
-
+pub struct Interrogation {
+    assigned_groups: BTreeSet<usize>,
+    unassigned_groups: BTreeSet<usize>,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct SubjectInterrogations<TeacherId: Identifier, GroupListId: Identifier> {
+pub struct InterrogationSlot {
+    slot_start: collomatique_time::SlotStart,
+    interrogations: Vec<Option<Interrogation>>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct TeacherInterrogations<InterrogationId: Identifier> {
+    interrogation_slots: HashMap<InterrogationId, InterrogationSlot>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct SubjectInterrogations<TeacherId: Identifier, InterrogationId: Identifier, GroupListId: Identifier> {
     duration: collomatique_time::NonZeroDurationInMinutes,
-    period: NonZeroUsize,
     group_list_id: GroupListId,
-    teacher_map: HashMap<TeacherId, TeacherInterrogations>
+    teacher_map: HashMap<TeacherId, TeacherInterrogations<InterrogationId>>,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct Colloscope<SubjectId: Identifier, TeacherId: Identifier, GroupListId: Identifier, StudentId: Identifier> {
-    subject_map: HashMap<SubjectId, SubjectInterrogations<TeacherId, GroupListId>>,
+pub struct Colloscope<SubjectId: Identifier, TeacherId: Identifier, InterrogationId: Identifier, GroupListId: Identifier, StudentId: Identifier> {
+    week_count: usize,
+    subject_map: HashMap<SubjectId, SubjectInterrogations<TeacherId, InterrogationId, GroupListId>>,
     group_lists: HashMap<GroupListId, GroupList<StudentId>>,
 }
 
