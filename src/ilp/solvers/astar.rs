49b71ae352aad6865cc5edbc29ce1e189cbf5e56
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:33:26 +0200

    Add stub A* solver (copy of Dijkstra for now)

---
+++ src/ilp/solvers/astar.rs
@@ -0,0 +1,59 @@
+#[cfg(test)]
+mod tests;
+
+use crate::ilp::ndtools::ConfigRepr;
+use crate::ilp::{Config, FeasableConfig, Problem};
+
+#[derive(Debug, Clone)]
+pub struct Solver<'a> {
+    problem: &'a Problem,
+}
+
+impl<'a> Solver<'a> {
+    pub fn new(problem: &'a Problem) -> Self {
+        Solver { problem }
+    }
+
+    fn distance_heuristic(&self, _config: &ConfigRepr) -> f32 {
+        0.
+    }
+}
+
+use super::FeasabilitySolver;
+use std::collections::BTreeSet;
+
+impl<'a> FeasabilitySolver<'a> for Solver<'a> {
+    fn restore_feasability_exclude(
+        &self,
+        config: &Config<'a>,
+        exclude_list: &BTreeSet<&FeasableConfig>,
+    ) -> Option<FeasableConfig<'a>> {
+        let config_repr = self.problem.mat_repr.config(config);
+
+        use std::collections::VecDeque;
+
+        let exclude_configs: BTreeSet<ConfigRepr<'_>> = exclude_list
+            .iter()
+            .map(|x| self.problem.mat_repr.config(x.inner()))
+            .collect();
+        let mut explored_configs = exclude_configs.clone();
+        let mut config_queue = VecDeque::new();
+        config_queue.push_back(config_repr);
+
+        while let Some(candidate) = config_queue.pop_front() {
+            if candidate.is_feasable() && !exclude_configs.contains(&candidate) {
+                return Some(unsafe { Config::from(candidate).into_feasable_unchecked() });
+            } else {
+                config_queue.extend(
+                    candidate
+                        .neighbours()
+                        .into_iter()
+                        .filter(|x| !explored_configs.contains(x)),
+                );
+                explored_configs.insert(candidate);
+            }
+        }
+
+        None
+    }
+}
