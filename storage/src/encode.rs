fe7f6f947b10693cf52499e631ba8dd9dbf93007
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:16 +0200

    Stub structure for encoding/decoding entries

---
+++ storage/src/encode.rs
@@ -0,0 +1,12 @@
+//! Encode submodule
+//!
+//! This module contains the logic that builds
+//! a [json::JsonData] from a [Data].
+//!
+//! The main function for this is [self::encode]
+
+use super::*;
+
+pub fn encode(_data: &Data) -> json::JsonData {
+    todo!()
+}
