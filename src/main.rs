3cf090b2635f8945d252f5987389a8fdd810e64b
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:33:11 +0200

    Adapt cli commands to automatically run GUI

--- src/main.rs
+++ src/main.rs
@@ -1,226 +1,54 @@
 use anyhow::{anyhow, Result};
 use clap::{Parser, Subcommand};
-use collomatique::backend::{sqlite, Logic};
 use collomatique::frontend::shell::CliCommand;
-use collomatique::frontend::state::AppState;
+
+mod cli;
+mod gui;
 
 #[derive(Debug, Parser)]
 #[command(
     version,
     about,
-    after_help = "If no command is provided, an interactive shell is opened."
+    after_help = "If no command is provided, a GUI is opened."
 )]
-/// Collomatique cli tool
+/// Collomatique - automatic colloscope creation tool
 struct Cli {
     /// Create new database - won't override an existing one
     #[arg(short, long, default_value_t = false)]
     create: bool,
-    /// Sqlite file (to open or create) that contains the database
-    db: std::path::PathBuf,
+    /// Sqlite file (to open or create) that contains the database.
+    /// A file must be given if using cli rather than gui
+    db: Option<std::path::PathBuf>,
+    /// Command to run on the file.
+    /// If no command is provided, the GUI will be opened.
+    /// You can open an interactive shell command line via the special shell command
     #[command(subcommand)]
-    command: Option<CliCommand>,
-}
-
-#[derive(Debug, Parser)]
-#[clap(disable_help_flag = true)]
-#[command(name = "")]
-struct ShellLine {
-    /// Command to run on the database
-    #[command(subcommand)]
-    command: ShellCommand,
+    command: Option<CliCommandOrShell>,
 }
 
 #[derive(Debug, Subcommand)]
-enum ShellCommand {
+enum CliCommandOrShell {
     #[command(flatten)]
     Global(CliCommand),
-    #[command(flatten)]
-    Extra(ShellExtraCommand),
+    /// Open a shell command line rather than opening a GUI
+    Shell,
 }
 
-#[derive(Debug, Subcommand)]
-enum ShellExtraCommand {
-    /// Undo last command
-    Undo,
-    /// Redo previously undone command
-    Redo,
-    PrintHistory,
-    /// Exit shell
-    Exit,
-}
-
-async fn connect_db(create: bool, path: &std::path::Path) -> Result<sqlite::Store> {
-    if create {
-        Ok(sqlite::Store::new_db(path).await?)
-    } else {
-        Ok(sqlite::Store::open_db(path).await?)
-    }
-}
-
-struct ReedCompleter {}
-
-impl reedline::Completer for ReedCompleter {
-    fn complete(&mut self, line: &str, pos: usize) -> Vec<reedline::Suggestion> {
-        use clap::CommandFactory;
-        use reedline::Span;
-        use std::ffi::OsString;
-
-        let mut cmd = ShellLine::command();
-        let args = shlex::Shlex::new(line);
-        let mut args = std::iter::once("".to_owned())
-            .chain(args)
-            .map(OsString::from)
-            .collect::<Vec<_>>();
-        if line.ends_with(' ') || line.is_empty() {
-            args.push(OsString::new());
-        }
-        let arg_index = args.len() - 1;
-        let span = Span::new(pos - args[arg_index].len(), pos);
-
-        let current_dir = std::env::current_dir().ok();
-        let current_dir_ref = current_dir.as_ref().map(|x| x.as_path());
-        let Ok(candidates) =
-            clap_complete::dynamic::complete(&mut cmd, args, arg_index, current_dir_ref)
-        else {
-            return vec![];
-        };
-
-        candidates
-            .into_iter()
-            .map(|c| reedline::Suggestion {
-                value: c.0.to_string_lossy().into_owned(),
-                description: c.1.map(|x| x.to_string()),
-                style: None,
-                extra: None,
-                span,
-                append_whitespace: true,
-            })
-            .collect()
-    }
-}
-
-async fn interactive_shell(app_state: &mut AppState<sqlite::Store>) -> Result<()> {
-    use nu_ansi_term::{Color, Style};
-    use reedline::{
-        DefaultHinter, Emacs, FileBackedHistory, IdeMenu, KeyCode, KeyModifiers, MenuBuilder,
-        Reedline, ReedlineEvent, ReedlineMenu,
-    };
-
-    let completer = Box::new(ReedCompleter {});
-    let completion_menu = Box::new(IdeMenu::default().with_name("completion_menu"));
-
-    let mut keybindings = reedline::default_emacs_keybindings();
-    keybindings.add_binding(
-        KeyModifiers::NONE,
-        KeyCode::Tab,
-        ReedlineEvent::UntilFound(vec![
-            ReedlineEvent::Menu("completion_menu".to_string()),
-            ReedlineEvent::MenuNext,
-        ]),
-    );
-
-    let mut rl = Reedline::create()
-        .with_completer(completer)
-        .with_menu(ReedlineMenu::EngineCompleter(completion_menu))
-        .with_hinter(Box::new(
-            DefaultHinter::default().with_style(Style::new().fg(Color::DarkGray).italic()),
-        ))
-        .with_edit_mode(Box::new(Emacs::new(keybindings)))
-        .with_history(Box::new(FileBackedHistory::new(10000).unwrap()));
-
-    loop {
-        match respond(&mut rl, app_state).await {
-            Ok(quit) => {
-                if quit {
-                    break;
-                }
-            }
-            Err(err) => {
-                use std::io::Write;
-                writeln!(std::io::stderr(), "{err}")?;
-                std::io::stderr().flush()?;
-            }
-        }
-    }
-    Ok(())
-}
-
-async fn respond(
-    rl: &mut reedline::Reedline,
-    app_state: &mut AppState<sqlite::Store>,
-) -> Result<bool> {
-    use collomatique::frontend::state::Manager;
-    use reedline::{DefaultPrompt, DefaultPromptSegment, Signal};
-
-    let mut prompt = DefaultPrompt::default();
-    prompt.left_prompt = DefaultPromptSegment::Basic("collomatique".to_owned());
-    prompt.right_prompt = DefaultPromptSegment::Empty;
-
-    let line = match rl.read_line(&prompt) {
-        Ok(Signal::Success(buffer)) => buffer,
-        Ok(Signal::CtrlC | Signal::CtrlD) => return Ok(true),
-        _ => return Err(anyhow!("Failed to read line. Input maybe invalid.")),
-    };
-
-    if line.trim().is_empty() {
-        return Ok(false);
-    }
-
-    let shell_command = match shlex::split(&line) {
-        Some(mut value) => {
-            value.insert(0, String::from(""));
-            match ShellLine::try_parse_from(value.iter().map(String::as_str)) {
-                Ok(c) => c,
-                Err(e) => return Err(e.into()),
-            }
-        }
-        None => return Err(anyhow!("Invalid input")),
-    };
-
-    let output = match shell_command.command {
-        ShellCommand::Global(command) => {
-            collomatique::frontend::shell::execute_cli_command(command, app_state).await?
-        }
-        ShellCommand::Extra(extra_command) => match extra_command {
-            ShellExtraCommand::Undo => {
-                app_state.undo().await?;
-                None
-            }
-            ShellExtraCommand::Redo => {
-                app_state.redo().await?;
-                None
-            }
-            ShellExtraCommand::PrintHistory => {
-                Some(format!("{:?}", app_state.get_aggregated_history()))
-            }
-            ShellExtraCommand::Exit => return Ok(true),
-        },
-    };
-    if let Some(msg) = output {
-        println!("{}", msg);
-    }
-
-    Ok(false)
-}
-
-#[tokio::main]
-async fn main() -> Result<()> {
+fn main() -> Result<()> {
     let args = Cli::parse();
 
-    let logic = Logic::new(connect_db(args.create, args.db.as_path()).await?);
-    let mut app_state = AppState::new(logic);
-
-    collomatique::frontend::python::initialize();
-
-    let Some(command) = args.command else {
-        interactive_shell(&mut app_state).await?;
-        return Ok(());
-    };
-
-    let output =
-        collomatique::frontend::shell::execute_cli_command(command, &mut app_state).await?;
-    if let Some(msg) = output {
-        print!("{}", msg);
+    match args.command {
+        Some(cmd) => {
+            let Some(db) = args.db else {
+                return Err(anyhow!(
+                    "You must specify a database file when using cli commands."
+                ));
+            };
+            cli::run_cli(args.create, db, cmd)?;
+        }
+        None => {
+            gui::run_gui(args.create, args.db)?;
+        }
     }
 
     Ok(())
