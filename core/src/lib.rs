3a16820ccbc8abc1be36b2c6a745b1c6617d275c
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:13 +0200

    Remove stub code and add first doc for collomatique-core

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,14 +1,22 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//! Collomatique-core
+//! ---
+//!
+//! This crate contains the main logic of Collomatique for solving colloscopes.
+//! The goal for this crate is to fulfill the role of a translator. It takes a description
+//! of a colloscope (or at least the various constraints of a colloscope) and returns
+//! an ILP problem as described by the crate [collomatique-ilp].
+//!
+//! Similarly, it can translate a solution of an ILP problem into the description of
+//! an actual colloscope and conversly, it can take the description of a colloscope
+//! and turn it into an ILP configuration. This is useful to check in real time if
+//! a colloscope satisfies all the constraints and helps when constructing a colloscope
+//! incrementally.
+//!
+//! This crate however does not expose a user-friendly interface. The reason is, to
+//! make the translation algorithm as thin as possible, and its verification as easy as
+//! possible, I strive to make the colloscopes constraints and the actual colloscopes
+//! representation the least redundant possible.
+//!
+//! It is the job of other crates to provide necessary utilities to make working the algorithm
+//! somewhat pleasant.
+//!
