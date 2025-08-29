411be4ad7daed1c76a1d2ba8b8a0e9aabb06f29b
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:32:17 +0200

    Implement week_pattern_get_all and week_pattern_get for sqlite::Storage

--- src/backend.rs
+++ src/backend.rs
@@ -1,3 +1,49 @@
 pub mod sqlite;
 
-pub trait Storage {}
+#[trait_variant::make(Send)]
+pub trait Storage {
+    type WeekPatternError;
+
+    async fn week_pattern_get_all(&self) -> Result<Vec<WeekPattern>, Self::WeekPatternError>;
+    async fn week_pattern_get(
+        &self,
+        index: WeekPatternId,
+    ) -> Result<WeekPattern, Self::WeekPatternError>;
+    async fn week_pattern_add(
+        &self,
+        pattern: WeekPattern,
+    ) -> Result<WeekPatternId, Self::WeekPatternError>;
+    async fn week_pattern_remove(&self, index: WeekPatternId)
+        -> Result<(), Self::WeekPatternError>;
+}
+
+use std::collections::BTreeSet;
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Week(u32);
+
+impl Week {
+    pub fn new(number: u32) -> Week {
+        Week(number)
+    }
+
+    pub fn get(&self) -> u32 {
+        self.0
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct WeekPattern {
+    pub name: String,
+    pub weeks: BTreeSet<Week>,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct WeekPatternId(usize);
+
+impl std::fmt::Display for WeekPatternId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)?;
+        Ok(())
+    }
+}
