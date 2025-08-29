9b1d5d9ada8f348ecc833fed8a4b4dd0c9319751
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:33:26 +0200

    First tentative interface for sa solver

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,14 @@ use collomatique::ilp::linexpr::Expr;
 
 fn main() {
     let pb = ilp::ProblemBuilder::new()
-        .add((2 * Expr::from("a") - 3 * Expr::from("b") + 4 * Expr::from("c")).leq(&(2 * Expr::from("a") - 5 * Expr::from("d"))))
-        .add((2 * Expr::from("c") - 3 * Expr::from("d") + 4 * Expr::from("e")).eq(&(-1 * Expr::from("e") + Expr::from("c"))))
+        .add((2 * Expr::from("a") - 3 * Expr::from("b") + 4 * Expr::from("c") - 3).leq(&(2 * Expr::from("a") - 5 * Expr::from("d"))))
+        .add((- Expr::from("a") + Expr::from("b") + 3 * Expr::from("c") + 3).leq(&(2 * Expr::from("a") - 5 * Expr::from("d"))))
+        .add((2 * Expr::from("c") - 3 * Expr::from("d") + 4 * Expr::from("e") + 2).eq(&(-1 * Expr::from("e") + Expr::from("c"))))
         .build();
 
     println!("{}", pb);
+
+    let solver = ilp::solvers::sa::Solver::new(&pb);
+
+    println!("{:?}", solver);
 }
