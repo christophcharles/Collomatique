5be9d4c4c28faa936973955a075cf06fb3ab1ae6
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:10 +0200

    Add iced dependancy

--- collomatique.nix
+++ collomatique.nix
@@ -5,8 +5,19 @@
     cbc,
     cmake,
     python3,
+    fontconfig,
+    freetype,
+    libglvnd,
+    libinput,
+    libxkbcommon,
+    makeBinaryWrapper,
+    mesa,
+    pkg-config,
+    vulkan-loader,
+    wayland,
+    xorg
 }:
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage rec {
     pname = "collomatique";
     version = "0.1.0";
 
@@ -20,13 +31,37 @@ rustPlatform.buildRustPackage {
         cmake
         rustPlatform.bindgenHook
         python3
+        pkg-config
+        makeBinaryWrapper
     ];
 
     buildInputs = [
         sqlite
         cbc
         python3
+        fontconfig
+        freetype
+        libglvnd
+        libinput
+        libxkbcommon
+        vulkan-loader
+        wayland
+        xorg.libX11
     ];
+
+    # LD_LIBRARY_PATH can be removed once tiny-xlib is bumped above 0.2.2
+    postInstall = ''
+        wrapProgram "$out/bin/${pname}" \
+            --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [
+                libxkbcommon
+                mesa.drivers
+                vulkan-loader
+                xorg.libX11
+                xorg.libXcursor
+                xorg.libXi
+                xorg.libXrandr
+            ]}
+    '';
 
     meta = {
         description = "Automatic colloscope building program";
