f13bd94a27c1ed3a837875af5def2fdad01f5c1f
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:33:26 +0200

    Add exclude list to FeasabilitySolver

--- src/ilp/solvers.rs
+++ src/ilp/solvers.rs
@@ -2,6 +2,17 @@ pub mod dijkstra;
 
 use super::Config;
 
+use std::collections::BTreeSet;
+
 pub trait FeasabilitySolver {
-    fn restore_feasability(&self, config: &Config) -> Option<Config>;
+    fn restore_feasability_exclude(
+        &self,
+        config: &Config,
+        exclude_list: &BTreeSet<&Config>,
+    ) -> Option<Config>;
+
+    fn restore_feasability(&self, config: &Config) -> Option<Config> {
+        let exclude_list = BTreeSet::new();
+        self.restore_feasability_exclude(config, &exclude_list)
+    }
 }
--- src/ilp/solvers/dijkstra.rs
+++ src/ilp/solvers/dijkstra.rs
@@ -1,7 +1,7 @@
 #[cfg(test)]
 mod tests;
 
-use crate::ilp::tools::MatRepr;
+use crate::ilp::tools::{ConfigRepr, MatRepr};
 use crate::ilp::{Config, Problem};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -18,14 +18,22 @@ impl<'a> Solver<'a> {
 }
 
 use super::FeasabilitySolver;
+use std::collections::BTreeSet;
 
 impl<'a> FeasabilitySolver for Solver<'a> {
-    fn restore_feasability(&self, config: &Config) -> Option<Config> {
+    fn restore_feasability_exclude(
+        &self,
+        config: &Config,
+        exclude_list: &BTreeSet<&Config>,
+    ) -> Option<Config> {
         let config_repr = self.mat_repr.config(config);
 
-        use std::collections::{BTreeSet, VecDeque};
+        use std::collections::VecDeque;
 
-        let mut explored_configs = BTreeSet::new();
+        let mut explored_configs: BTreeSet<ConfigRepr<'_, '_>> = exclude_list
+            .iter()
+            .map(|x| self.mat_repr.config(x))
+            .collect();
         let mut config_queue = VecDeque::new();
         config_queue.push_back(config_repr);
 
