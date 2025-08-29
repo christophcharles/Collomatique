541a37c34f2ceaa337c9b2c7d89ff36060384ae2
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:17 +0200

    Move rpc engine to gtk4 crate to avoid circular dependancies

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -5,7 +5,6 @@
 
 use std::io::Write;
 
-use anyhow::anyhow;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
@@ -88,82 +87,24 @@ fn wait_for_msg() -> String {
     buffer
 }
 
-fn send_raw_msg(msg: &str) -> Result<OutMsg, String> {
+pub fn send_raw_msg(msg: &str) -> Result<OutMsg, String> {
     print!("{}\r\n", msg);
     std::io::stdout().flush().expect("no error on flush");
     let data = wait_for_msg();
     OutMsg::from_text_msg(&data)
 }
 
-fn send_rpc(cmd: CmdMsg) -> Result<OutMsg, String> {
+pub fn send_rpc(cmd: CmdMsg) -> Result<OutMsg, String> {
     let msg = CompleteCmdMsg::CmdMsg(cmd).into_text_msg();
     send_raw_msg(&msg)
 }
 
-fn wait_for_init_msg() -> Result<InitMsg, String> {
+pub fn wait_for_init_msg() -> Result<InitMsg, String> {
     let data = wait_for_msg();
     InitMsg::from_text_msg(&data)
 }
 
-fn send_exit() {
+pub fn send_exit() {
     print!("{}\r\n", CompleteCmdMsg::GracefulExit.into_text_msg());
     std::io::stdout().flush().expect("no error on flush");
-}
-
-/// Main RPC Engine function
-///
-/// Runs the RPC engine through stdin/stdout
-pub fn run_rpc_engine() -> Result<(), anyhow::Error> {
-    eprintln!("Waiting for initial payload...");
-    let init_msg = match wait_for_init_msg() {
-        Ok(x) => x,
-        Err(e) => return Err(anyhow!("Unknown initial payload: {}", e)),
-    };
-    eprintln!("Payload received!");
-
-    match init_msg {
-        InitMsg::RpcTest => run_rpc_test()?,
-        InitMsg::RunPythonScript(script) => collomatique_python::run_python_script(script)?,
-    }
-
-    eprintln!("Exiting...");
-    send_exit();
-
-    Ok(())
-}
-
-fn run_rpc_test() -> Result<(), anyhow::Error> {
-    eprintln!("Running RPC test...");
-    for i in 1..=100 {
-        eprintln!("Msg {}", i);
-        match i % 3 {
-            1 => match send_rpc(CmdMsg::Success) {
-                Ok(rep) => {
-                    if rep != OutMsg::Ack {
-                        return Err(anyhow!("Invalid response to valid command!"));
-                    }
-                }
-                Err(e) => return Err(anyhow!("Invalid response: {}", e)),
-            },
-            2 => match send_rpc(CmdMsg::Warning) {
-                Ok(rep) => {
-                    if rep != OutMsg::Ack {
-                        return Err(anyhow!("Invalid response to valid command!"));
-                    }
-                }
-                Err(e) => return Err(anyhow!("Invalid response: {}", e)),
-            },
-            _ => match send_raw_msg("This is not a valid command...") {
-                Ok(rep) => {
-                    if rep != OutMsg::Invalid {
-                        return Err(anyhow!("Ack response to invalid command!"));
-                    }
-                }
-                Err(e) => return Err(anyhow!("Invalid response: {}", e)),
-            },
-        }
-        std::thread::sleep(std::time::Duration::from_millis(250));
-    }
-    eprintln!("End of RPC test");
-    Ok(())
 }
