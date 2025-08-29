2d689c9cdcf2bdc181b5e6b4a360e381d7381578
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:14 +0200

    Add stub simple_schedule example

--- core/src/examples.rs
+++ core/src/examples.rs
@@ -3,3 +3,5 @@
 //! This module defines a few simple examples of implementations for [crate::BaseConstraints] and [crate::ExtraConstraints].
 //! This is used mostly for documentation on their usage together with [crate::ProblemBuilder] and [crate::Problem]
 //! but also in order to be able to write tests.
+
+pub mod simple_schedule;
---
+++ core/src/examples/simple_schedule.rs
@@ -0,0 +1,12 @@
+//! Simple schedule example
+//!
+//! This module implements a *very* simple example of scheduling problem to illustrate
+//! the usage of [crate::BaseConstraints] and [crate::ExtraConstraints].
+//!
+//! The problem being implemented here is a very simple scheduling problem where
+//! we have a few courses (let's n of them) and a few class or student groups (let's
+//! note m their number) than should attend them.
+//!
+//! The courses are supposed to happen all at the same time and all classes should attend
+//! each course exactly once. That's it. There are only two variables to our problem: n and m.
+//! And we only have to complete a very simple schedule.
