9a472daee6e98ffd4a4c64f0fed272956f162ad2
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:17 +0200

    First Python engine

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -1,3 +1,20 @@
 //! Collomatique-python crate
 //!
 //! This crate contains the code to run python code
+
+use pyo3::prelude::*;
+
+pub fn run_python_script(script: String) -> anyhow::Result<()> {
+    pyo3::prepare_freethreaded_python();
+    let cscript = std::ffi::CString::new(script)?;
+    let flush_script = std::ffi::CString::new(
+        "import sys
+sys.stdout.flush()
+sys.stderr.flush()",
+    )?;
+    Python::with_gil(|py| {
+        py.run(&cscript, None, None)?;
+        py.run(&flush_script, None, None)?;
+        Ok(())
+    })
+}
