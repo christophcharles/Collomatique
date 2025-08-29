96e8a41da5c4980ec26d059dd8b2504959c0ed75
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:32:21 +0200

    Add stub periodicity_cuts

--- src/frontend/translator.rs
+++ src/frontend/translator.rs
@@ -189,6 +189,7 @@ impl GenColloscopeTranslator {
             week_count: data.general_data.week_count,
             interrogations_per_week: data.general_data.interrogations_per_week.clone(),
             max_interrogations_per_day: data.general_data.max_interrogations_per_day,
+            periodicity_cuts: BTreeSet::new(),
         }
     }
 }
