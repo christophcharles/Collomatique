b879417f1597434ec448e6ed76842ed0cb223a54
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:14 +0200

    Fix doc

--- core/src/examples/simple_schedule.rs
+++ core/src/examples/simple_schedule.rs
@@ -4,9 +4,9 @@
 //! the usage of [crate::BaseConstraints] and [crate::ExtraConstraints].
 //!
 //! The problem being implemented here is a very simple scheduling problem where
-//! we have a few courses (let's n of them) and a few class or student groups (let's
+//! we have a few courses (let's n of them) and a few groups of students (let's
 //! note m their number) than should attend them.
 //!
-//! The courses are supposed to happen all at the same time and all classes should attend
+//! The courses are supposed to happen all at the same time and all groups should attend
 //! each course exactly once. That's it. There are only two variables to our problem: n and m.
 //! And we only have to complete a very simple schedule.
