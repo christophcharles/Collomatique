e3803d1246452c916243e091dc640e2bd76545fc
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:17 +0200

    Add a lot of From implementations

--- core/src/rpc/cmd_msg.rs
+++ core/src/rpc/cmd_msg.rs
@@ -8,7 +8,26 @@ pub enum CmdMsg {
     GeneralPlanning(GeneralPlanningCmdMsg),
 }
 
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+impl From<crate::ops::UpdateOp> for CmdMsg {
+    fn from(value: crate::ops::UpdateOp) -> Self {
+        match value {
+            crate::ops::UpdateOp::GeneralPlanning(op) => CmdMsg::GeneralPlanning(op.into()),
+        }
+    }
+}
+
+impl CmdMsg {
+    pub fn promote(
+        self,
+        data: &collomatique_state_colloscopes::Data,
+    ) -> Result<crate::ops::UpdateOp, ErrorMsg> {
+        Ok(match self {
+            CmdMsg::GeneralPlanning(op) => crate::ops::UpdateOp::GeneralPlanning(op.promote(data)?),
+        })
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
 pub struct MsgPeriodId(pub u64);
 
 impl From<collomatique_state_colloscopes::PeriodId> for MsgPeriodId {
--- core/src/rpc/cmd_msg/general_planning.rs
+++ core/src/rpc/cmd_msg/general_planning.rs
@@ -1,3 +1,5 @@
+use crate::rpc::error_msg::general_planning::GeneralPlanningError;
+
 use super::*;
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
@@ -39,5 +41,56 @@ impl From<crate::ops::GeneralPlanningUpdateOp> for GeneralPlanningCmdMsg {
                 GeneralPlanningCmdMsg::UpdateWeekStatus(id.into(), week, new_status)
             }
         }
+    }
+}
+
+impl GeneralPlanningCmdMsg {
+    pub fn promote(
+        self,
+        data: &collomatique_state_colloscopes::Data,
+    ) -> Result<crate::ops::GeneralPlanningUpdateOp, GeneralPlanningError> {
+        use crate::ops::GeneralPlanningUpdateOp;
+        Ok(match self {
+            GeneralPlanningCmdMsg::DeleteFirstWeek => GeneralPlanningUpdateOp::DeleteFirstWeek,
+            GeneralPlanningCmdMsg::UpdateFirstWeek(monday_date) => {
+                let Some(date) = collomatique_time::NaiveMondayDate::new(monday_date) else {
+                    return Err(error_msg::UpdateFirstWeekError::DateIsNotAMonday.into());
+                };
+                GeneralPlanningUpdateOp::UpdateFirstWeek(date)
+            }
+            GeneralPlanningCmdMsg::AddNewPeriod(week_count) => {
+                GeneralPlanningUpdateOp::AddNewPeriod(week_count)
+            }
+            GeneralPlanningCmdMsg::UpdatePeriodWeekCount(id, week_count) => {
+                let Some(period_id) = data.validate_period_id(id.0) else {
+                    return Err(error_msg::UpdatePeriodWeekCountError::InvalidPeriodId(id).into());
+                };
+                GeneralPlanningUpdateOp::UpdatePeriodWeekCount(period_id, week_count)
+            }
+            GeneralPlanningCmdMsg::DeletePeriod(id) => {
+                let Some(period_id) = data.validate_period_id(id.0) else {
+                    return Err(error_msg::UpdatePeriodWeekCountError::InvalidPeriodId(id).into());
+                };
+                GeneralPlanningUpdateOp::DeletePeriod(period_id)
+            }
+            GeneralPlanningCmdMsg::CutPeriod(id, remaining_weeks) => {
+                let Some(period_id) = data.validate_period_id(id.0) else {
+                    return Err(error_msg::UpdatePeriodWeekCountError::InvalidPeriodId(id).into());
+                };
+                GeneralPlanningUpdateOp::CutPeriod(period_id, remaining_weeks)
+            }
+            GeneralPlanningCmdMsg::MergeWithPreviousPeriod(id) => {
+                let Some(period_id) = data.validate_period_id(id.0) else {
+                    return Err(error_msg::UpdatePeriodWeekCountError::InvalidPeriodId(id).into());
+                };
+                GeneralPlanningUpdateOp::MergeWithPreviousPeriod(period_id)
+            }
+            GeneralPlanningCmdMsg::UpdateWeekStatus(id, week, new_status) => {
+                let Some(period_id) = data.validate_period_id(id.0) else {
+                    return Err(error_msg::UpdatePeriodWeekCountError::InvalidPeriodId(id).into());
+                };
+                GeneralPlanningUpdateOp::UpdateWeekStatus(period_id, week, new_status)
+            }
+        })
     }
 }
