dd2ee2ceb5f95095b5fe9ebb87c09ad0a970f0d2
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:15 +0200

    Stub core functionnality

---
+++ core/src/ops.rs
@@ -0,0 +1,29 @@
+//! Ops module
+//!
+//! This modules defines all the operations (that means atomic modification)
+//! we can do on the data
+
+use super::*;
+
+/// Operation enumeration
+///
+/// This is the list of all possible operations on [super::Data]
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Op {
+    /// Operation on the student list
+    Student(StudentOp),
+}
+
+/// Student operation enumeration
+///
+/// This is the list of all possible operations related to the
+/// student list we can do on a [super::Data]
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum StudentOp {
+    /// Add a new student
+    Add(Student),
+    /// Remove an existing student identified through its id
+    Remove(StudentId),
+    /// Update the data on an existing student
+    Update(StudentId, Student),
+}
