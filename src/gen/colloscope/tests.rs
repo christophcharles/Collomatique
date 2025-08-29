c1b98168cb46130465f946f5c82a0ff7549da5b0
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:30:58 +0200

    Add unit tests for gen::colloscope::Error::SlotOverlapsNextDay

--- src/gen/colloscope/tests.rs
+++ src/gen/colloscope/tests.rs
@@ -23,6 +23,52 @@ fn trivial_validated_data() {
         Ok(expected_result)
     );
 }
+
+#[test]
+fn simple_validated_data() {
+    let general = GeneralData {
+        teacher_count: 1,
+        week_count: NonZeroU32::new(1).unwrap(),
+    };
+
+    let subjects = vec![Subject {
+        students_per_interrogation: NonZeroU32::new(2).unwrap()..=NonZeroU32::new(3).unwrap(),
+        period: NonZeroU32::new(2).unwrap(),
+        duration: NonZeroU32::new(60).unwrap(),
+        interrogations: vec![Interrogation {
+            teacher: 0,
+            slots: vec![SlotStart {
+                week: 0,
+                weekday: time::Weekday::Monday,
+                start_time: time::Time::from_hm(23, 0).unwrap(),
+            }],
+        }],
+    }];
+    let incompatibilities = vec![Incompatibility {
+        slots: vec![Slot {
+            duration: NonZeroU32::new(60).unwrap(),
+            start: SlotStart {
+                week: 0,
+                weekday: time::Weekday::Monday,
+                start_time: time::Time::from_hm(23, 0).unwrap(),
+            },
+        }],
+    }];
+    let students = StudentList::new();
+
+    let expected_result = ValidatedData {
+        general: general.clone(),
+        subjects: subjects.clone(),
+        incompatibilities: incompatibilities.clone(),
+        students: students.clone(),
+    };
+
+    assert_eq!(
+        ValidatedData::new(general, subjects, incompatibilities, students),
+        Ok(expected_result)
+    );
+}
+
 #[test]
 fn invalid_students_per_interrogation() {
     let general = GeneralData {
@@ -49,5 +95,55 @@ fn invalid_students_per_interrogation() {
     assert_eq!(
         ValidatedData::new(general, subjects, incompatibilities, students),
         Err(Error::InvalidStudentsPerInterrogationRange)
+    );
+}
+
+#[test]
+fn slot_overlaps_next_day() {
+    let general = GeneralData {
+        teacher_count: 1,
+        week_count: NonZeroU32::new(1).unwrap(),
+    };
+
+    let subjects = vec![Subject {
+        students_per_interrogation: NonZeroU32::new(2).unwrap()..=NonZeroU32::new(3).unwrap(),
+        period: NonZeroU32::new(2).unwrap(),
+        duration: NonZeroU32::new(60).unwrap(),
+        interrogations: vec![Interrogation {
+            teacher: 0,
+            slots: vec![SlotStart {
+                week: 0,
+                weekday: time::Weekday::Monday,
+                start_time: time::Time::from_hm(23, 1).unwrap(),
+            }],
+        }],
+    }];
+    let incompatibilities = IncompatibilityList::new();
+    let students = StudentList::new();
+
+    assert_eq!(
+        ValidatedData::new(general, subjects, incompatibilities, students),
+        Err(Error::SlotOverlapsNextDay)
+    );
+
+    let general = GeneralData {
+        teacher_count: 0,
+        week_count: NonZeroU32::new(1).unwrap(),
+    };
+    let subjects = SubjectList::new();
+    let incompatibilities = vec![Incompatibility {
+        slots: vec![Slot {
+            duration: NonZeroU32::new(60).unwrap(),
+            start: SlotStart {
+                week: 0,
+                weekday: time::Weekday::Monday,
+                start_time: time::Time::from_hm(23, 1).unwrap(),
+            },
+        }],
+    }];
+    let students = StudentList::new();
+    assert_eq!(
+        ValidatedData::new(general, subjects, incompatibilities, students),
+        Err(Error::SlotOverlapsNextDay)
     );
 }
