e6ad2db3dea0e9050ec86746612a784ea726b15b
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:13 +0200

    Fix doc comments

--- core/src/time.rs
+++ core/src/time.rs
@@ -5,27 +5,27 @@
 
 use std::num::NonZeroU32;
 
-// DurationInMinutes obviously represents a duration in minutes.
-//
-// The type is useful because it gives information on the type of data
-// represented. Also, it garantees that the underlying duration is
-// at least one minute.
-//
-// Crucially all durations have always positive span.
-//
-// On top of that it implements a few utility functions to interact with chrono.
+/// DurationInMinutes obviously represents a duration in minutes.
+///
+/// The type is useful because it gives information on the type of data
+/// represented. Also, it garantees that the underlying duration is
+/// at least one minute.
+///
+/// Crucially all durations have always positive span.
+///
+/// On top of that it implements a few utility functions to interact with chrono.
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub struct NonZeroDurationInMinutes(NonZeroU32);
 
 impl NonZeroDurationInMinutes {
-    // Creates a new [NonZeroDurationInMinutes] from a number of minutes
-    //
-    // Returns `None` if the value is zero.
+    /// Creates a new [NonZeroDurationInMinutes] from a number of minutes
+    ///
+    /// Returns `None` if the value is zero.
     pub fn new(value: u32) -> Option<NonZeroDurationInMinutes> {
         Some(NonZeroDurationInMinutes(NonZeroU32::new(value)?))
     }
 
-    // Returns the corresponding [chrono::TimeDelta].
+    /// Returns the corresponding [chrono::TimeDelta].
     pub fn time_delta(self) -> chrono::TimeDelta {
         chrono::TimeDelta::minutes(self.0.get().into())
     }
@@ -37,8 +37,8 @@ impl From<NonZeroDurationInMinutes> for chrono::TimeDelta {
     }
 }
 
-// Encapsulates a [chrono::Weekday] and gives it a default ordering
-// (monday is the lowest and sunday the biggest day)
+/// Encapsulates a [chrono::Weekday] and gives it a default ordering
+/// (monday is the lowest and sunday the biggest day)
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Weekday(chrono::Weekday);
 
@@ -75,51 +75,51 @@ impl std::ops::DerefMut for Weekday {
     }
 }
 
-// Type representing the beginning of a slot in time
-//
-// A slot starts on a given week, on a given weekday
-// and a certain time.
+/// Type representing the beginning of a slot in time
+///
+/// A slot starts on a given week, on a given weekday
+/// and a certain time.
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct SlotStart {
-    // Week of the colloscope the slot starts on
+    /// Week of the colloscope the slot starts on
     pub week: u32,
 
-    // Weekday the slot starts on
+    /// Weekday the slot starts on
     pub weekday: Weekday,
 
-    // The time the slot starts
-    //
-    // The time uses [chrono::NaiveTime] as we don't
-    // really need localization at this point in the algorithm
-    // Obviously, all the times in the colloscope should be
-    // expressed in the same timezone.
+    /// The time the slot starts
+    ///
+    /// The time uses [chrono::NaiveTime] as we don't
+    /// really need localization at this point in the algorithm
+    /// Obviously, all the times in the colloscope should be
+    /// expressed in the same timezone.
     pub start_time: chrono::NaiveTime,
 }
 
-// Type representing a slot in time, with it its start time but
-// also the corresponding duration.
-//
-// The resolution is at most the minute for the duration and a slot
-// cannot be zero minutes long.
-//
-// A slot cannot cross a day boundary
+/// Type representing a slot in time, with it its start time but
+/// also the corresponding duration.
+///
+/// The resolution is at most the minute for the duration and a slot
+/// cannot be zero minutes long.
+///
+/// A slot cannot cross a day boundary
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub struct SlotWithDuration {
-    // start of the slot described by a [SlotStart]
+    /// start of the slot described by a [SlotStart]
     start: SlotStart,
 
-    // duration of the slot
-    //
-    // It is expressed in minutes using [time::NonZeroDurationInMinutes]
-    // and cannot be zero.
+    /// duration of the slot
+    ///
+    /// It is expressed in minutes using [time::NonZeroDurationInMinutes]
+    /// and cannot be zero.
     duration: NonZeroDurationInMinutes,
 }
 
 impl SlotWithDuration {
-    // Creates a new slot with duration
-    //
-    // Returns `None` if the duration is too big and the slot would cross the
-    // day boundary
+    /// Creates a new slot with duration
+    ///
+    /// Returns `None` if the duration is too big and the slot would cross the
+    /// day boundary
     pub fn new(start: SlotStart, duration: NonZeroDurationInMinutes) -> Option<SlotWithDuration> {
         let (end, ignored_seconds) = start.start_time.overflowing_add_signed(duration.into());
 
@@ -142,16 +142,16 @@ impl SlotWithDuration {
         Some(SlotWithDuration { start, duration })
     }
 
-    // Returns the end time of a slot
-    //
-    // This end time is just past the end of the slot. So
-    // if this slot is an interrogation, the student is actually free
-    // at the end time.
+    /// Returns the end time of a slot
+    ///
+    /// This end time is just past the end of the slot. So
+    /// if this slot is an interrogation, the student is actually free
+    /// at the end time.
     pub fn end_time(&self) -> chrono::NaiveTime {
         self.start.start_time + self.duration.time_delta()
     }
 
-    // Checks if two slots (with duration) overlap
+    /// Checks if two slots (with duration) overlap
     pub fn overlap_with(&self, other: &SlotWithDuration) -> bool {
         if self.start.week != other.start.week {
             return false;
