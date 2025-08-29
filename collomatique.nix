b1424d64678556c980efe9cfea14b11f32356baf
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:33:11 +0200

    Fix nix files for iced

--- collomatique.nix
+++ collomatique.nix
@@ -49,17 +49,27 @@ rustPlatform.buildRustPackage rec {
         xorg.libX11
     ];
 
+    # Force linking to libEGL, which is always dlopen()ed, and to
+    # libwayland-client, which is always dlopen()ed except by the
+    # obscure winit backend.
+    RUSTFLAGS = map (a: "-C link-arg=${a}") [
+        "-Wl,--push-state,--no-as-needed"
+        "-lEGL"
+        "-lwayland-client"
+        "-Wl,--pop-state"
+    ];
+
+    shellHook = ''
+        export LD_LIBRARY_PATH="${lib.makeLibraryPath [
+            xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr vulkan-loader libxkbcommon mesa.drivers wayland
+            ]}:$LD_LIBRARY_PATH"
+    '';
+
     # LD_LIBRARY_PATH can be removed once tiny-xlib is bumped above 0.2.2
     postInstall = ''
         wrapProgram "$out/bin/${pname}" \
             --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [
-                libxkbcommon
-                mesa.drivers
-                vulkan-loader
-                xorg.libX11
-                xorg.libXcursor
-                xorg.libXi
-                xorg.libXrandr
+            xorg.libX11 xorg.libXcursor xorg.libXi xorg.libXrandr vulkan-loader libxkbcommon mesa.drivers wayland
             ]}
     '';
 
