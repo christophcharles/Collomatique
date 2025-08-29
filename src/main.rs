e3377c271624057a1a39b06bb1b588d1c66a46df
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:33:28 +0200

    Move to snake-case for db

--- src/main.rs
+++ src/main.rs
@@ -26,122 +26,121 @@ async fn create_db(db_url: &str) -> Result<SqliteConnection> {
 
     sqlx::query(
         r#"
-CREATE TABLE "Colloscopes" (
-    "ColloscopeId"	INTEGER NOT NULL,
-    "Name"	TEXT NOT NULL,
-    PRIMARY KEY("ColloscopeId" AUTOINCREMENT)
+CREATE TABLE "colloscopes" (
+    "colloscope_id"	INTEGER NOT NULL,
+    "name"	TEXT NOT NULL,
+    PRIMARY KEY("colloscope_id" AUTOINCREMENT)
 );
 
-CREATE TABLE "CourseIncompatItems" (
-    "CourseIncompatId"	INTEGER NOT NULL,
-    "WeekPatternId"	INTEGER NOT NULL,
-    "StartDay"	INTEGER NOT NULL,
-    "StartTime"	INTEGER NOT NULL,
-    "Duration"	INTEGER NOT NULL,
-    PRIMARY KEY("CourseIncompatId","WeekPatternId","StartDay","StartTime","Duration")
+CREATE TABLE "course_incompat_items" (
+	"course_incompat_id"	INTEGER NOT NULL,
+	"week_pattern_id"	INTEGER NOT NULL,
+	"start_day"	INTEGER NOT NULL,
+	"start_time"	INTEGER NOT NULL,
+	"duration"	INTEGER NOT NULL,
+	PRIMARY KEY("course_incompat_id","week_pattern_id","start_day","start_time","duration")
 );
 
-CREATE TABLE "CourseIncompats" (
-    "CourseIncompatId"	INTEGER NOT NULL,
-    "Name"	TEXT NOT NULL,
-    PRIMARY KEY("CourseIncompatId")
+CREATE TABLE "course_incompats" (
+	"course_incompat_id"	INTEGER NOT NULL,
+	"name"	TEXT NOT NULL,
+	PRIMARY KEY("course_incompat_id")
 );
 
-CREATE TABLE "GeneralData" (
-    "Name"	TEXT NOT NULL,
-    "Value"	INTEGER,
-    PRIMARY KEY("Name")
+CREATE TABLE "general_data" (
+	"name"	TEXT NOT NULL,
+	"value"	INTEGER,
+	PRIMARY KEY("name")
 );
 
-CREATE TABLE "GroupingIncompats" (
-    "Id1"	INTEGER NOT NULL,
-    "Id2"	INTEGER NOT NULL,
-    PRIMARY KEY("Id1","Id2")
+CREATE TABLE "grouping_incompats" (
+	"id1"	INTEGER NOT NULL,
+	"id2"	INTEGER NOT NULL,
+	PRIMARY KEY("id1","id2")
 );
 
-CREATE TABLE "GroupingItems" (
-    "GroupingId"	INTEGER NOT NULL,
-    "TimeSlotId"	INTEGER NOT NULL,
-    PRIMARY KEY("GroupingId","TimeSlotId")
+CREATE TABLE "grouping_items" (
+	"grouping_id"	INTEGER NOT NULL,
+	"time_slot_id"	INTEGER NOT NULL,
+	PRIMARY KEY("grouping_id","time_slot_id")
 );
 
-CREATE TABLE "Groupings" (
-	"GroupingId"	INTEGER NOT NULL,
-	"Name"	TEXT NOT NULL,
-	PRIMARY KEY("GroupingId" AUTOINCREMENT)
+CREATE TABLE "groupings" (
+	"grouping_id"	INTEGER NOT NULL,
+	"name"	TEXT NOT NULL,
+	PRIMARY KEY("grouping_id" AUTOINCREMENT)
 );
 
-CREATE TABLE "StudentIncompats" (
-	"StudentId"	INTEGER NOT NULL,
-	"IncompatId"	INTEGER NOT NULL,
-	PRIMARY KEY("StudentId","IncompatId")
+CREATE TABLE "student_incompats" (
+	"student_id"	INTEGER NOT NULL,
+	"incompat_id"	INTEGER NOT NULL,
+	PRIMARY KEY("student_id","incompat_id")
 );
 
-CREATE TABLE "StudentSubjects" (
-	"StudentId"	INTEGER NOT NULL,
-	"SubjectId"	INTEGER NOT NULL,
-	PRIMARY KEY("SubjectId","StudentId")
+CREATE TABLE "student_subjects" (
+	"student_id"	INTEGER NOT NULL,
+	"subject_id"	INTEGER NOT NULL,
+	PRIMARY KEY("subject_id","student_id")
 );
 
-CREATE TABLE "Students" (
-	"StudentId"	INTEGER NOT NULL,
-	"Surname"	TEXT NOT NULL,
-	"Firstname"	TEXT NOT NULL,
-	"Email"	TEXT,
-	"Phone"	TEXT,
-	PRIMARY KEY("StudentId" AUTOINCREMENT)
+CREATE TABLE "students" (
+	"student_id"	INTEGER NOT NULL,
+	"surname"	TEXT NOT NULL,
+	"firstname"	TEXT NOT NULL,
+	"email"	TEXT,
+	"phone"	TEXT,
+	PRIMARY KEY("student_id" AUTOINCREMENT)
 );
 
-CREATE TABLE "SubjectGroups" (
-	"SubjectGroupId"	INTEGER NOT NULL,
-	"Name"	TEXT NOT NULL,
-	"Optional"	INTEGER NOT NULL,
-	PRIMARY KEY("SubjectGroupId" AUTOINCREMENT)
+CREATE TABLE "subject_groups" (
+	"subject_group_id"	INTEGER NOT NULL,
+	"name"	TEXT NOT NULL,
+	"optional"	INTEGER NOT NULL,
+	PRIMARY KEY("subject_group_id" AUTOINCREMENT)
 );
 
-CREATE TABLE "Subjects" (
-	"SubjectId"	INTEGER NOT NULL,
-	"Name"	TEXT NOT NULL,
-	"SubjectGroup"	INTEGER NOT NULL,
-	"Duration"	INTEGER NOT NULL,
-	"CourseIncompatId"	INTEGER,
-	"MinStudentsPerSlot"	INTEGER NOT NULL,
-	"MaxStudentsPerSlot"	INTEGER NOT NULL,
-	"Period"	INTEGER NOT NULL,
-	"PeriodIsStrict"	INTEGER NOT NULL,
-	PRIMARY KEY("SubjectId" AUTOINCREMENT)
+CREATE TABLE "subjects" (
+	"subject_id"	INTEGER NOT NULL,
+	"name"	TEXT NOT NULL,
+	"subject_group"	INTEGER NOT NULL,
+	"duration"	INTEGER NOT NULL,
+	"course_incompat_id"	INTEGER,
+	"min_students_per_slot"	INTEGER NOT NULL,
+	"max_students_per_slot"	INTEGER NOT NULL,
+	"period"	INTEGER NOT NULL,
+	"period_is_strict"	INTEGER NOT NULL,
+	PRIMARY KEY("subject_id" AUTOINCREMENT)
 );
 
-CREATE TABLE "Teachers" (
-	"TeacherId"	INTEGER NOT NULL,
-	"Surname"	TEXT NOT NULL,
-	"Firstname"	TEXT NOT NULL,
-	"Contact"	TEXT NOT NULL,
-	PRIMARY KEY("TeacherId" AUTOINCREMENT)
+CREATE TABLE "teachers" (
+	"teacher_id"	INTEGER NOT NULL,
+	"surname"	TEXT NOT NULL,
+	"firstname"	TEXT NOT NULL,
+	"contact"	TEXT NOT NULL,
+	PRIMARY KEY("teacher_id" AUTOINCREMENT)
 );
 
-CREATE TABLE "TimeSlots" (
-	"TimeSlotId"	INTEGER NOT NULL,
-	"SubjectId"	INTEGER NOT NULL,
-	"TeacherId"	INTEGER NOT NULL,
-	"StartDay"	INTEGER NOT NULL,
-	"StartTime"	INTEGER NOT NULL,
-	"WeekPattern"	INTEGER NOT NULL,
-	PRIMARY KEY("TimeSlotId" AUTOINCREMENT)
+CREATE TABLE "time_slots" (
+	"time_slot_id"	INTEGER NOT NULL,
+	"subject_id"	INTEGER NOT NULL,
+	"teacher_id"	INTEGER NOT NULL,
+	"start_day"	INTEGER NOT NULL,
+	"start_time"	INTEGER NOT NULL,
+	"week_pattern"	INTEGER NOT NULL,
+	PRIMARY KEY("time_slot_id" AUTOINCREMENT)
 );
 
-CREATE TABLE "WeekPatterns" (
-	"WeekPatternId"	INTEGER NOT NULL,
-	"Name"	TEXT NOT NULL,
-	PRIMARY KEY("WeekPatternId")
+CREATE TABLE "week_patterns" (
+	"week_pattern_id"	INTEGER NOT NULL,
+	"name"	TEXT NOT NULL,
+	PRIMARY KEY("week_pattern_id")
 );
 
-CREATE TABLE "Weeks" (
-	"WeekPatternId"	INTEGER NOT NULL,
-	"Week"	INTEGER NOT NULL,
-	PRIMARY KEY("WeekPatternId","Week")
-);
-        "#,
+CREATE TABLE "weeks" (
+	"week_pattern_id"	INTEGER NOT NULL,
+	"week"	INTEGER NOT NULL,
+	PRIMARY KEY("week_pattern_id","week")
+);"#,
     )
     .execute(&mut db)
     .await?;
@@ -176,7 +175,7 @@ async fn main() -> Result<()> {
     let args = Args::parse();
 
     let mut db = connect_db(args.create, args.db.as_path()).await?;
-    let result = sqlx::query!("SELECT * FROM Students")
+    let result = sqlx::query!("SELECT * FROM students")
         .fetch_all(&mut db)
         .await?;
 
