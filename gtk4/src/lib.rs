3f8e735e49d96faba5b3e991782e503c56fb7de6
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:15 +0200

    Stub window for gtk4 gui

---
+++ gtk4/src/lib.rs
@@ -0,0 +1,41 @@
+use gtk::prelude::GtkWindowExt;
+use relm4::{gtk, ComponentParts, ComponentSender, SimpleComponent};
+
+pub struct AppModel {}
+
+#[derive(Debug)]
+pub enum AppInput {}
+
+pub struct AppWidgets {}
+
+#[relm4::component(pub)]
+impl SimpleComponent for AppModel {
+    type Input = AppInput;
+    type Output = ();
+    type Init = ();
+
+    view! {
+        #[root]
+        root_window = gtk::ApplicationWindow {
+            set_default_width: 800,
+            set_default_height: 600,
+            set_title: Some("Collomatique"),
+            #[wrap(Some)]
+            set_child = &gtk::Label {
+                set_label: "Stub",
+            },
+        }
+    }
+
+    fn init(
+        _params: Self::Init,
+        root: Self::Root,
+        _sender: ComponentSender<Self>,
+    ) -> ComponentParts<Self> {
+        let model = AppModel {};
+        let widgets = view_output!();
+        ComponentParts { model, widgets }
+    }
+
+    fn update(&mut self, _message: Self::Input, _sender: ComponentSender<Self>) {}
+}
