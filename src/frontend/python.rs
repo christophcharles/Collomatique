7711f994d94d9e0a6aebd866772b74347ee0664e
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:33:33 +0200

    Simple python integration with collomatique module

--- src/frontend/python.rs
+++ src/frontend/python.rs
@@ -1,16 +1,35 @@
 use pyo3::prelude::*;
 
+#[pyfunction]
+fn test_function() -> String {
+    "test from rust code".into()
+}
+
+#[pymodule]
+fn collomatique(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(test_function, m)?)
+}
+
+pub fn initialize() {
+    pyo3::append_to_inittab!(collomatique);
+    pyo3::prepare_freethreaded_python();
+}
+
 pub fn python_test() -> PyResult<()> {
     Python::with_gil(|py| {
-        let fun: Py<PyAny> = PyModule::from_code_bound(
+        let python_code = PyModule::from_code_bound(
             py,
-            "def example():
-                print('Test')",
+            "import collomatique
+def example():
+    data = collomatique.test_function()
+    print(data)",
             "",
             "",
-        )?
-        .getattr("example")?
-        .into();
+        )?;
+
+        let fun: Py<PyAny> = python_code
+            .getattr("example")?
+            .into();
 
         fun.call0(py)?;
 
