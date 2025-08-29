40d1d75bfcaaaac54109667ec2c7d51a0f13c57d
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-29 06:31:15 +0200

    Restructure collomatique-core

--- core/src/colloscopes.rs
+++
@@ -1,9 +0,0 @@
-//! Colloscopes module
-//! 
-//! This modules contain the code that defines the various colloscopes constraints.
-//! It builds on [collomatique_solver::generics] and [collomatique_solver::solver] and uses its interfaces
-//! to build a *nice-enough* interface for automatic solving of colloscopes.
-//! 
-
-pub mod structs;
-
---
+++ core/src/constraints.rs
@@ -0,0 +1,11 @@
+//! Constraints module
+//! 
+//! This module defines all the necessary constraints to define a colloscope.
+//! 
+//! The main constraints are described in the module [main]. They define what a colloscope
+//! is but do not add anything extra.
+//! 
+//! Other possible constraints are defined in the various other submodules and allow
+//! a finer description of your colloscope problem.
+
+pub mod main;
