30823e165e930cdd61fa753fa9bb4fed95235152
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-08-30 11:59:13 +0200

    Add strict limits to general settings

--- state-colloscopes/src/settings.rs
+++ state-colloscopes/src/settings.rs
@@ -2,6 +2,20 @@
 //!
 //! This module defines the relevant types to describes general settings
 
+use std::num::NonZeroU32;
+
 /// Description of the general settings
 #[derive(Clone, Debug, PartialEq, Eq, Default)]
-pub struct GeneralSettings {}
+pub struct GeneralSettings {
+    /// Strict limits to impose during resolution
+    pub strict_limits: StrictLimits,
+}
+
+/// Strict limits in resolution
+#[derive(Clone, Debug, PartialEq, Eq, Default)]
+pub struct StrictLimits {
+    /// Number of interrogations for each student per week
+    pub interrogations_per_week: Option<std::ops::Range<u32>>,
+    /// maximum number of interrogation in a single day for each student
+    pub max_interrogations_per_day: Option<NonZeroU32>,
+}
