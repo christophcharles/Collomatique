e6f49e3dc1de1292b8c550be987896cee57a9c21
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-10-12 18:24:17 +0200

    Add ColloscopeParams for Python API

--- python/src/glue/params.rs
+++ python/src/glue/params.rs
@@ -178,3 +178,185 @@ impl TryFrom<collomatique_state_colloscopes::colloscope_params::GeneralParameter
         })
     }
 }
+
+#[pyclass]
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct ColloscopeParameters {
+    #[pyo3(get)]
+    pub periods: Vec<ColloscopePeriod>,
+    #[pyo3(get)]
+    pub periods_first_week: Option<time::NaiveMondayDate>,
+    #[pyo3(get)]
+    pub subjects: Vec<subjects::ColloscopeSubject>,
+    #[pyo3(get)]
+    pub teachers: BTreeMap<ColloscopeTeacherId, ColloscopeTeacher>,
+    #[pyo3(get)]
+    pub students: BTreeMap<ColloscopeStudentId, ColloscopeStudent>,
+    #[pyo3(get)]
+    pub assignments:
+        BTreeMap<ColloscopePeriodId, BTreeMap<ColloscopeSubjectId, BTreeSet<ColloscopeStudentId>>>,
+    #[pyo3(get)]
+    pub week_patterns: BTreeMap<ColloscopeWeekPatternId, WeekPattern>,
+    #[pyo3(get)]
+    pub slots: BTreeMap<subjects::ColloscopeSubjectId, Vec<slots::ColloscopeSlot>>,
+    #[pyo3(get)]
+    pub incompats:
+        BTreeMap<incompatibilities::ColloscopeIncompatId, incompatibilities::ColloscopeIncompat>,
+    #[pyo3(get)]
+    pub group_lists: BTreeMap<group_lists::ColloscopeGroupListId, group_lists::ColloscopeGroupList>,
+    #[pyo3(get)]
+    pub group_lists_associations: BTreeMap<
+        ColloscopePeriodId,
+        BTreeMap<ColloscopeSubjectId, group_lists::ColloscopeGroupListId>,
+    >,
+    #[pyo3(get)]
+    pub rules: BTreeMap<rules::ColloscopeRuleId, rules::ColloscopeRule>,
+    #[pyo3(get)]
+    pub settings: settings::GeneralSettings,
+}
+
+impl TryFrom<collomatique_state_colloscopes::colloscope_params::ColloscopeParameters>
+    for ColloscopeParameters
+{
+    type Error = PyErr;
+    fn try_from(
+        value: collomatique_state_colloscopes::colloscope_params::ColloscopeParameters,
+    ) -> PyResult<Self> {
+        Ok(ColloscopeParameters {
+            periods: value
+                .periods
+                .ordered_period_list
+                .into_iter()
+                .map(|(period_id, weeks_status)| ColloscopePeriod {
+                    id: period_id.into(),
+                    weeks_status,
+                })
+                .collect(),
+            periods_first_week: match value.periods.first_week {
+                Some(week) => Some(week.into()),
+                None => None,
+            },
+            subjects: value
+                .subjects
+                .ordered_subject_list
+                .into_iter()
+                .map(|(subject_id, subject)| ColloscopeSubject {
+                    id: subject_id.into(),
+                    parameters: subject.parameters.into(),
+                    excluded_periods: subject
+                        .excluded_periods
+                        .into_iter()
+                        .map(|period_id| period_id.into())
+                        .collect(),
+                })
+                .collect(),
+            teachers: value
+                .teachers
+                .teacher_map
+                .into_iter()
+                .map(|(teacher_id, teacher)| (teacher_id.into(), teacher.into()))
+                .collect(),
+            students: value
+                .students
+                .student_map
+                .into_iter()
+                .map(|(student_id, student)| (student_id.into(), student.into()))
+                .collect(),
+            assignments: value
+                .assignments
+                .period_map
+                .into_iter()
+                .map(|(period_id, period_assignements)| {
+                    (
+                        period_id.into(),
+                        period_assignements
+                            .subject_map
+                            .into_iter()
+                            .map(|(subject_id, students)| {
+                                (
+                                    subject_id.into(),
+                                    students.into_iter().map(|id| id.into()).collect(),
+                                )
+                            })
+                            .collect(),
+                    )
+                })
+                .collect(),
+            week_patterns: value
+                .week_patterns
+                .week_pattern_map
+                .into_iter()
+                .map(|(week_pattern_id, week_pattern)| {
+                    (
+                        week_pattern_id.into(),
+                        WeekPattern {
+                            name: week_pattern.name,
+                            weeks: week_pattern.weeks,
+                        },
+                    )
+                })
+                .collect(),
+            slots: value
+                .slots
+                .subject_map
+                .into_iter()
+                .map(|(subject_id, subject_slots)| {
+                    (
+                        subject_id.into(),
+                        subject_slots
+                            .ordered_slots
+                            .into_iter()
+                            .map(|(slot_id, slot)| slots::ColloscopeSlot {
+                                id: slot_id.into(),
+                                parameters: slot.into(),
+                            })
+                            .collect(),
+                    )
+                })
+                .collect(),
+            incompats: value
+                .incompats
+                .incompat_map
+                .into_iter()
+                .map(|(incompat_id, incompat)| (incompat_id.into(), incompat.into()))
+                .collect(),
+            group_lists: value
+                .group_lists
+                .group_list_map
+                .into_iter()
+                .map(|(group_list_id, group_list)| (group_list_id.into(), group_list.into()))
+                .collect(),
+            group_lists_associations: value
+                .group_lists
+                .subjects_associations
+                .into_iter()
+                .map(|(period_id, subject_map)| {
+                    (
+                        period_id.into(),
+                        subject_map
+                            .into_iter()
+                            .map(|(subject_id, group_list_id)| {
+                                (subject_id.into(), group_list_id.into())
+                            })
+                            .collect(),
+                    )
+                })
+                .collect(),
+            rules: value
+                .rules
+                .rule_map
+                .into_iter()
+                .map(|(rule_id, rule)| {
+                    PyResult::Ok((
+                        ColloscopeRuleId::from(rule_id),
+                        rules::ColloscopeRule {
+                            name: rule.name,
+                            logic_rule: rule.desc.try_into()?,
+                        },
+                    ))
+                })
+                .collect::<Result<_, _>>()?,
+            settings: value.settings.into(),
+        })
+    }
+}
