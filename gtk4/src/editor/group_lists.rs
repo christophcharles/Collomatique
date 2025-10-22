01af105d60135f627479a3efe117b2bc24b40da6
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-10-22 15:39:35 +0200

    Prototype interface for group lists

--- gtk4/src/editor/group_lists.rs
+++ gtk4/src/editor/group_lists.rs
@@ -1,5 +1,6 @@
-use gtk::prelude::{BoxExt, OrientableExt, WidgetExt};
-use relm4::gtk;
+use adw::prelude::PreferencesRowExt;
+use gtk::prelude::{BoxExt, ButtonExt, OrientableExt, WidgetExt};
+use relm4::{adw, gtk};
 use relm4::{Component, ComponentParts, ComponentSender, RelmWidgetExt};
 
 use collomatique_ops::GroupListsUpdateOp;
@@ -61,12 +62,131 @@ impl Component for GroupLists {
                 set_hexpand: true,
                 set_orientation: gtk::Orientation::Vertical,
                 set_margin_all: 5,
-                set_spacing: 5,
-                gtk::Label {
-                    set_margin_top: 10,
-                    set_halign: gtk::Align::Start,
-                    set_label: "<big><b>En développement...</b></big>",
-                    set_use_markup: true,
+                set_spacing: 30,
+                gtk::Box {
+                    set_hexpand: true,
+                    set_orientation: gtk::Orientation::Vertical,
+                    set_spacing: 10,
+                    gtk::Label {
+                        set_halign: gtk::Align::Start,
+                        set_label: "Listes de groupes",
+                        set_attributes: Some(&gtk::pango::AttrList::from_string("weight bold, scale 1.2").unwrap()),
+                    },
+                    gtk::ListBox {
+                        set_hexpand: true,
+                        add_css_class: "boxed-list",
+                        set_selection_mode: gtk::SelectionMode::None,
+                        append = &gtk::Box {
+                            set_hexpand: true,
+                            set_margin_all: 5,
+                            set_orientation: gtk::Orientation::Horizontal,
+                            set_spacing: 5,
+                            gtk::Button {
+                                set_icon_name: "edit-symbolic",
+                                add_css_class: "flat",
+                            },
+                            gtk::Separator {
+                                set_orientation: gtk::Orientation::Vertical,
+                            },
+                            gtk::Label {
+                                set_halign: gtk::Align::Start,
+                                set_xalign: 0.,
+                                set_margin_start: 5,
+                                set_margin_end: 5,
+                                set_label: "Liste 1",
+                                set_size_request: (150, -1),
+                            },
+                            gtk::Button {
+                                set_icon_name: "view-list-bullet-symbolic",
+                                add_css_class: "flat",
+                            },
+                            gtk::Separator {
+                                set_orientation: gtk::Orientation::Vertical,
+                            },
+                            gtk::Label {
+                                set_halign: gtk::Align::Start,
+                                set_xalign: 0.,
+                                set_margin_start: 5,
+                                set_margin_end: 5,
+                                set_label: "<b>Élèves par groupe :</b> 2 à 3",
+                                set_use_markup: true,
+                            },
+                            gtk::Separator {
+                                set_orientation: gtk::Orientation::Horizontal,
+                                add_css_class: "spacer",
+                            },
+                            gtk::Label {
+                                set_halign: gtk::Align::Start,
+                                set_xalign: 0.,
+                                set_margin_start: 5,
+                                set_margin_end: 5,
+                                set_label: "<b>Nombre de groupes :</b> 7 à 8",
+                                set_use_markup: true,
+                            },
+                            gtk::Box {
+                                set_hexpand: true,
+                            },
+                            gtk::Separator {
+                                set_orientation: gtk::Orientation::Vertical,
+                            },
+                            gtk::Button {
+                                set_icon_name: "edit-delete",
+                                add_css_class: "flat",
+                            },
+                        },
+                    },
+                    gtk::Button {
+                        set_margin_top: 10,
+                        adw::ButtonContent {
+                            set_icon_name: "edit-add",
+                            set_label: "Ajouter une liste de groupes",
+                        },
+                    }
+                },
+                gtk::Box {
+                    set_hexpand: true,
+                    set_orientation: gtk::Orientation::Vertical,
+                    set_spacing: 30,
+                    gtk::Box {
+                        set_hexpand: true,
+                        set_orientation: gtk::Orientation::Vertical,
+                        set_spacing: 10,
+                        gtk::Label {
+                            set_halign: gtk::Align::Start,
+                            set_label: "Associations pour la période 1",
+                            set_attributes: Some(&gtk::pango::AttrList::from_string("weight bold, scale 1.2").unwrap()),
+                        },
+                        adw::PreferencesGroup {
+                            set_margin_all: 5,
+                            set_hexpand: true,
+                            adw::ComboRow {
+                                set_title: "Français",
+                            },
+                            adw::ComboRow {
+                                set_title: "Maths",
+                            },
+                        },
+                    },
+                    gtk::Box {
+                        set_hexpand: true,
+                        set_orientation: gtk::Orientation::Vertical,
+                        set_spacing: 10,
+                        gtk::Label {
+                            set_halign: gtk::Align::Start,
+                            set_label: "Associations pour la période 2",
+                            set_attributes: Some(&gtk::pango::AttrList::from_string("weight bold, scale 1.2").unwrap()),
+                        },
+                        adw::PreferencesGroup {
+                            set_margin_all: 5,
+                            set_hexpand: true,
+                            adw::ComboRow {
+                                set_title: "Français",
+                            },
+                            adw::ComboRow {
+                                set_title: "Maths",
+                            },
+                        },
+                    }
                 },
             }
         }
