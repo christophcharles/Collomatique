7e5077e6ddacac3dc3ec864dd4fa44bf7ea68ab7
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-11-30 15:50:22 +0100

    Add constraints_from_different_scripts test

--- collo-ml/src/problem/tests.rs
+++ collo-ml/src/problem/tests.rs
@@ -276,3 +276,111 @@ fn multiple_function_calls() {
         "W should be 1"
     );
 }
+
+#[test]
+fn constraints_from_different_scripts() {
+    #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+    enum Var {
+        V,
+        W,
+    }
+
+    impl EvalVar for Var {
+        fn field_schema() -> HashMap<String, Vec<crate::traits::FieldType>> {
+            HashMap::from([("V".to_string(), vec![]), ("W".to_string(), vec![])])
+        }
+        fn fix(&self) -> Option<f64> {
+            None
+        }
+        fn vars() -> std::collections::BTreeMap<Self, collomatique_ilp::Variable> {
+            BTreeMap::from([
+                (Var::V, collomatique_ilp::Variable::binary()),
+                (Var::W, collomatique_ilp::Variable::binary()),
+            ])
+        }
+    }
+
+    impl<T: EvalObject> TryFrom<&ExternVar<T>> for Var {
+        type Error = VarConversionError;
+        fn try_from(value: &ExternVar<T>) -> Result<Self, Self::Error> {
+            match value.name.as_str() {
+                "V" => {
+                    if value.params.len() != 0 {
+                        return Err(VarConversionError::WrongParameterCount {
+                            name: "V".into(),
+                            expected: 0,
+                            found: value.params.len(),
+                        });
+                    }
+                    Ok(Var::V)
+                }
+                "W" => {
+                    if value.params.len() != 0 {
+                        return Err(VarConversionError::WrongParameterCount {
+                            name: "W".into(),
+                            expected: 0,
+                            found: value.params.len(),
+                        });
+                    }
+                    Ok(Var::W)
+                }
+                _ => Err(VarConversionError::Unknown(value.name.clone())),
+            }
+        }
+    }
+
+    let env = NoObjectEnv {};
+    let mut pb_builder =
+        ProblemBuilder::<NoObject, Var>::new(&env).expect("NoObject and Var should be compatible");
+
+    // Add constraint from first script
+    let warnings = pb_builder
+        .add_constraints(
+            Script {
+                name: "script1".into(),
+                content: r#"
+                    pub let c1() -> Constraint = $V() === 1;
+                "#
+                .into(),
+            },
+            vec![("c1".to_string(), vec![])],
+        )
+        .expect("Should compile first script");
+
+    assert!(warnings.is_empty(), "Unexpected warnings: {:?}", warnings);
+
+    // Add constraint from second script
+    let warnings = pb_builder
+        .add_constraints(
+            Script {
+                name: "script2".into(),
+                content: r#"
+                    pub let c2() -> Constraint = $W() === 1;
+                "#
+                .into(),
+            },
+            vec![("c2".to_string(), vec![])],
+        )
+        .expect("Should compile second script");
+
+    assert!(warnings.is_empty(), "Unexpected warnings: {:?}", warnings);
+
+    let problem = pb_builder.build();
+
+    let solver = collomatique_ilp::solvers::coin_cbc::CbcSolver::new();
+    use collomatique_ilp::solvers::Solver;
+    let sol_opt = solver.solve(problem.get_inner_problem());
+
+    let sol = sol_opt.expect("There should be a solution");
+
+    assert_eq!(
+        sol.get(ProblemVar::Base(Var::V)),
+        Some(1.0),
+        "V should be 1"
+    );
+    assert_eq!(
+        sol.get(ProblemVar::Base(Var::W)),
+        Some(1.0),
+        "W should be 1"
+    );
+}
