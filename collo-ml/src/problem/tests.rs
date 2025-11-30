c9d0ed1f16646f26f4b149a199faebbf6b63d773
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-11-30 22:25:26 +0100

    Add test for constraints and objectives in the same script

--- collo-ml/src/problem/tests.rs
+++ collo-ml/src/problem/tests.rs
@@ -1,6 +1,7 @@
 use super::*;
 
 mod complex_reification;
+mod constraints_and_objectives_at_once;
 mod constraints_list;
 mod errors;
 mod eval_var_fix;
---
+++ collo-ml/src/problem/tests/constraints_and_objectives_at_once.rs
@@ -0,0 +1,106 @@
+use crate::eval::{NoObject, NoObjectEnv};
+use collomatique_ilp::ObjectiveSense;
+
+use super::*;
+
+#[test]
+fn constraints_and_objectives_same_call() {
+    #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+    enum Var {
+        V,
+        W,
+    }
+
+    impl EvalVar for Var {
+        fn field_schema() -> HashMap<String, Vec<crate::traits::FieldType>> {
+            HashMap::from([("V".to_string(), vec![]), ("W".to_string(), vec![])])
+        }
+        fn fix(&self) -> Option<f64> {
+            None
+        }
+        fn vars() -> std::collections::BTreeMap<Self, collomatique_ilp::Variable> {
+            BTreeMap::from([
+                (Var::V, collomatique_ilp::Variable::binary()),
+                (Var::W, collomatique_ilp::Variable::binary()),
+            ])
+        }
+    }
+
+    impl<T: EvalObject> TryFrom<&ExternVar<T>> for Var {
+        type Error = VarConversionError;
+        fn try_from(value: &ExternVar<T>) -> Result<Self, Self::Error> {
+            match value.name.as_str() {
+                "V" => {
+                    if value.params.len() != 0 {
+                        return Err(VarConversionError::WrongParameterCount {
+                            name: "V".into(),
+                            expected: 0,
+                            found: value.params.len(),
+                        });
+                    }
+                    Ok(Var::V)
+                }
+                "W" => {
+                    if value.params.len() != 0 {
+                        return Err(VarConversionError::WrongParameterCount {
+                            name: "W".into(),
+                            expected: 0,
+                            found: value.params.len(),
+                        });
+                    }
+                    Ok(Var::W)
+                }
+                _ => Err(VarConversionError::Unknown(value.name.clone())),
+            }
+        }
+    }
+
+    let env = NoObjectEnv {};
+    let mut pb_builder =
+        ProblemBuilder::<NoObject, Var>::new(&env).expect("NoObject and Var should be compatible");
+
+    // Add both constraints and objectives from the same script in one call
+    let warnings = pb_builder
+        .add_constraints_and_objectives(
+            Script {
+                name: "combined".into(),
+                content: r#"
+                    pub let constraint() -> Constraint = $V() + $W() === 1;
+                    pub let objective() -> LinExpr = $V();
+                "#
+                .into(),
+            },
+            vec![("constraint".to_string(), vec![])],
+            vec![(
+                "objective".to_string(),
+                vec![],
+                1.0,
+                ObjectiveSense::Maximize,
+            )],
+        )
+        .expect("Should compile");
+
+    assert!(warnings.is_empty(), "Unexpected warnings: {:?}", warnings);
+
+    let problem = pb_builder.build();
+
+    let solver = collomatique_ilp::solvers::coin_cbc::CbcSolver::new();
+    use collomatique_ilp::solvers::Solver;
+    let sol_opt = solver.solve(problem.get_inner_problem());
+
+    let sol = sol_opt.expect("There should be a solution");
+
+    // Constraint: V + W === 1
+    // Objective: Maximize V
+    // Should select V=1, W=0
+    assert_eq!(
+        sol.get(ProblemVar::Base(Var::V)),
+        Some(1.0),
+        "V should be 1 (maximized)"
+    );
+    assert_eq!(
+        sol.get(ProblemVar::Base(Var::W)),
+        Some(0.0),
+        "W should be 0"
+    );
+}
