842b1f215925e8d997a688c114f13b72210fdf27
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-11-30 16:25:44 +0100

    Add simple test for global reification

--- collo-ml/src/problem/tests.rs
+++ collo-ml/src/problem/tests.rs
@@ -511,3 +511,112 @@ fn internal_reification() {
         "X should be 0"
     );
 }
+
+#[test]
+fn global_reified_variables_basic() {
+    #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+    enum Var {
+        V,
+        W,
+    }
+
+    impl EvalVar for Var {
+        fn field_schema() -> HashMap<String, Vec<crate::traits::FieldType>> {
+            HashMap::from([("V".to_string(), vec![]), ("W".to_string(), vec![])])
+        }
+        fn fix(&self) -> Option<f64> {
+            None
+        }
+        fn vars() -> std::collections::BTreeMap<Self, collomatique_ilp::Variable> {
+            BTreeMap::from([
+                (Var::V, collomatique_ilp::Variable::binary()),
+                (Var::W, collomatique_ilp::Variable::binary()),
+            ])
+        }
+    }
+
+    impl<T: EvalObject> TryFrom<&ExternVar<T>> for Var {
+        type Error = VarConversionError;
+        fn try_from(value: &ExternVar<T>) -> Result<Self, Self::Error> {
+            match value.name.as_str() {
+                "V" => {
+                    if value.params.len() != 0 {
+                        return Err(VarConversionError::WrongParameterCount {
+                            name: "V".into(),
+                            expected: 0,
+                            found: value.params.len(),
+                        });
+                    }
+                    Ok(Var::V)
+                }
+                "W" => {
+                    if value.params.len() != 0 {
+                        return Err(VarConversionError::WrongParameterCount {
+                            name: "W".into(),
+                            expected: 0,
+                            found: value.params.len(),
+                        });
+                    }
+                    Ok(Var::W)
+                }
+                _ => Err(VarConversionError::Unknown(value.name.clone())),
+            }
+        }
+    }
+
+    let env = NoObjectEnv {};
+    let mut pb_builder =
+        ProblemBuilder::<NoObject, Var>::new(&env).expect("NoObject and Var should be compatible");
+
+    // First, define a reified variable globally
+    let warnings = pb_builder
+        .add_reified_variables(
+            Script {
+                name: "reified_defs".into(),
+                content: r#"
+                    pub let v_is_one() -> Constraint = $V() === 1;
+                "#
+                .into(),
+            },
+            vec![("v_is_one".to_string(), "VIsOne".to_string())],
+        )
+        .expect("Should compile reified variables");
+
+    assert!(warnings.is_empty(), "Unexpected warnings: {:?}", warnings);
+
+    // Now use the reified variable in a constraint
+    let warnings = pb_builder
+        .add_constraints(
+            Script {
+                name: "use_reified".into(),
+                content: r#"
+                    pub let use_it() -> Constraint = $VIsOne() === 1 and $W() === 0;
+                "#
+                .into(),
+            },
+            vec![("use_it".to_string(), vec![])],
+        )
+        .expect("Should compile constraints");
+
+    assert!(warnings.is_empty(), "Unexpected warnings: {:?}", warnings);
+
+    let problem = pb_builder.build();
+
+    let solver = collomatique_ilp::solvers::coin_cbc::CbcSolver::new();
+    use collomatique_ilp::solvers::Solver;
+    let sol_opt = solver.solve(problem.get_inner_problem());
+
+    let sol = sol_opt.expect("There should be a solution");
+
+    // VIsOne === 1 means V === 1 must hold
+    assert_eq!(
+        sol.get(ProblemVar::Base(Var::V)),
+        Some(1.0),
+        "V should be 1"
+    );
+    assert_eq!(
+        sol.get(ProblemVar::Base(Var::W)),
+        Some(0.0),
+        "W should be 0"
+    );
+}
