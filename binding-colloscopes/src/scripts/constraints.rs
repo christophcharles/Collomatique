e865337a2c879bdca72b774ebafa9da5acb00257
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-12-05 10:56:31 +0100

    Add order for group filling

--- binding-colloscopes/src/scripts/constraints.rs
+++ binding-colloscopes/src/scripts/constraints.rs
@@ -4,10 +4,17 @@ pub const GROUP_COUNT_PER_INTERROGATION: &'static str =
 pub const STUDENTS_PER_GROUP: &'static str =
     include_str!("constraints/students_per_group.collo-ml");
 
+pub const GROUPS_FILLED_BY_ASCENDING_ORDER: &'static str =
+    include_str!("constraints/groups_filled_by_ascending_order.collo-ml");
+
 pub const DEFAULT_CONSTRAINT_LIST: &'static [(&'static str, &'static str)] = &[
     (
         "group_count_per_interrogation",
         GROUP_COUNT_PER_INTERROGATION,
     ),
     ("students_per_group", STUDENTS_PER_GROUP),
+    (
+        "groups_filled_by_ascending_order",
+        GROUPS_FILLED_BY_ASCENDING_ORDER,
+    ),
 ];
