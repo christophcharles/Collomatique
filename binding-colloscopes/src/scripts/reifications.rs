2d58cea71cb28f32b63d5d0e4959aa2f21be63ba
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-12-05 10:45:03 +0100

    Add comment for reification order

--- binding-colloscopes/src/scripts/reifications.rs
+++ binding-colloscopes/src/scripts/reifications.rs
@@ -6,6 +6,7 @@ pub const STUDENT_IN_GROUP: &'static str = include_str!("reifications/student_in
 pub const GROUP_HAS_STUDENTS: &'static str =
     include_str!("reifications/group_has_students.collo-ml");
 
+// Warning! Order matters!
 pub const DEFAULT_REIFICATION_LIST: &'static [(&'static str, &'static str)] = &[
     ("interrogation_has_groups", INTERROGATION_HAS_GROUPS),
     ("student_in_group", STUDENT_IN_GROUP),
