27469a1e35a820ddd5a6cfdae2a84804461cc353
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-12-05 17:31:07 +0100

    Add student_at_interrogation reified variable

--- binding-colloscopes/src/scripts/reifications.rs
+++ binding-colloscopes/src/scripts/reifications.rs
@@ -6,9 +6,13 @@ pub const STUDENT_IN_GROUP: &'static str = include_str!("reifications/student_in
 pub const GROUP_HAS_STUDENTS: &'static str =
     include_str!("reifications/group_has_students.collo-ml");
 
+pub const STUDENT_AT_INTERROGATION: &'static str =
+    include_str!("reifications/student_at_interrogation.collo-ml");
+
 // Warning! Order matters!
 pub const DEFAULT_REIFICATION_LIST: &'static [(&'static str, &'static str)] = &[
     ("interrogation_has_groups", INTERROGATION_HAS_GROUPS),
     ("student_in_group", STUDENT_IN_GROUP),
     ("group_has_students", GROUP_HAS_STUDENTS),
+    ("student_at_interrogation", STUDENT_AT_INTERROGATION),
 ];
