94c4e0b2f54fe90f399534786878ccca4954344b
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2025-12-20 11:10:21 +0100

    Remove old deprecated solver code

--- solver-glue/src/lib.rs
+++
@@ -1,7 +0,0 @@
-//! Solver module
-//!
-//! This module contains the code for translating the information
-//! defined in states to actually ilp problems.
-//!
-
-pub mod colloscopes;
--- solver/src/examples/simple_schedule/tests.rs
+++
@@ -1,7 +0,0 @@
-use super::*;
-
-mod general_constraints;
-mod partial_solution;
-mod single_constraints;
-mod solution_conversion;
-mod variables;
