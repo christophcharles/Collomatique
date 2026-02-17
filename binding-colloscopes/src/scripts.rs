6052d4b22427e88750cfaf1dcd4c708183d56163
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2026-02-17 15:38:21 +0100

    Add `AmountForEveryArbitraryBlock` periodicity constraints

--- binding-colloscopes/src/scripts.rs
+++ binding-colloscopes/src/scripts.rs
@@ -81,6 +81,10 @@ pub const MODULES: &[(&str, &str)] = &[
         include_str!("scripts/collomatique_one_interrogation_at_once.collo-ml"),
     ),
     (
+        "collomatique_periodicity_amount_for_every_arbitrary_block",
+        include_str!("scripts/collomatique_periodicity_amount_for_every_arbitrary_block.collo-ml"),
+    ),
+    (
         "collomatique_periodicity_amount_in_year",
         include_str!("scripts/collomatique_periodicity_amount_in_year.collo-ml"),
     ),
