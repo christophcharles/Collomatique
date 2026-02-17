0e25cb8d8ec78f6d2ec80b5aefea083d2441b8d3
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2026-02-17 13:19:59 +0100

    Add AmountInYear total count constraint (min separation still TODO)

--- binding-colloscopes/src/scripts.rs
+++ binding-colloscopes/src/scripts.rs
@@ -81,6 +81,10 @@ pub const MODULES: &[(&str, &str)] = &[
         include_str!("scripts/collomatique_one_interrogation_at_once.collo-ml"),
     ),
     (
+        "collomatique_periodicity_amount_in_year",
+        include_str!("scripts/collomatique_periodicity_amount_in_year.collo-ml"),
+    ),
+    (
         "collomatique_periodicity_helpers",
         include_str!("scripts/collomatique_periodicity_helpers.collo-ml"),
     ),
