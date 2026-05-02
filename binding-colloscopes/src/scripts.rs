2e25b63c5f1e35dd6c8b2a5ba9bd590862aa5245
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2026-05-02 21:03:48 +0200

    Remove collomatique_students_have_groups.collo-ml script

--- binding-colloscopes/src/scripts.rs
+++ binding-colloscopes/src/scripts.rs
@@ -128,10 +128,6 @@ pub const MODULES: &[(&str, &str)] = &[
         include_str!("scripts/collomatique_balancing_period_rotation.collo-ml"),
     ),
     (
-        "collomatique_students_have_groups",
-        include_str!("scripts/collomatique_students_have_groups.collo-ml"),
-    ),
-    (
         "collomatique_students_per_group_for_subject",
         include_str!("scripts/collomatique_students_per_group_for_subject.collo-ml"),
     ),
