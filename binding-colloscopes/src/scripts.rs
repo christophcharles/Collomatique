46087ebec37eef8cfb257c1e58a229d60214da25
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2026-05-04 15:55:43 +0200

    Remove collomatique_balancing_avoid_twice_in_a_row.collo-ml script

--- binding-colloscopes/src/scripts.rs
+++ binding-colloscopes/src/scripts.rs
@@ -64,10 +64,6 @@ pub const MODULES: &[(&str, &str)] = &[
         include_str!("scripts/collomatique_balancing_slot_rotation.collo-ml"),
     ),
     (
-        "collomatique_balancing_avoid_twice_in_a_row",
-        include_str!("scripts/collomatique_balancing_avoid_twice_in_a_row.collo-ml"),
-    ),
-    (
         "collomatique_balancing_year_rotation",
         include_str!("scripts/collomatique_balancing_year_rotation.collo-ml"),
     ),
