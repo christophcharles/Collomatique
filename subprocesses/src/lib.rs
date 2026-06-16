1e061f7ba681a53871241bac82aa56f4cc057eb8
Christoph Charles <23055925+christophcharles@users.noreply.github.com>
2026-06-16 19:16:34 +0200

    Add spawn_strategy and StrategySubprocess parent-side API

--- subprocesses/src/lib.rs
+++ subprocesses/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod ilp_solver;
 mod process;
 mod process_manager;
+pub mod strategy_solver;
 pub mod subprocess_solve_backend;
 mod worker;
 mod worker_manager;
@@ -12,6 +13,9 @@ pub use ilp_solver::{
 pub use process::ProcessStatus;
 pub use process::{OutputData, OutputEntry, ProcessEvent, ProcessId, ProcessState, StdinWriter};
 pub use process_manager::ProcessManager;
+pub use strategy_solver::{
+    StrategyProgress, StrategyResult, StrategyStatus, StrategySubprocess, spawn_strategy,
+};
 pub use subprocess_solve_backend::SubprocessSolveBackend;
 pub use worker::{WorkerEvent, WorkerId, WorkerState};
 pub use worker_manager::WorkerManager;
---
+++ subprocesses/src/strategy_solver.rs
@@ -0,0 +1,148 @@
+use std::io::Write;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::{Arc, Mutex};
+
+use collomatique_rpc::{EncodedMsg, InitMsg, ResultMsg, SerializedStrategyRequest, StrategyMsg};
+use collomatique_strategies::{StrategyKind, StrategyRequest};
+
+use crate::process::StdinWriter;
+use crate::worker::{WorkerEvent, WorkerId};
+use crate::worker_manager::WorkerManager;
+
+#[derive(Debug, Clone)]
+pub struct StrategyProgress {
+    pub message: String,
+    pub best_objective: Option<f64>,
+    pub feasible: Option<bool>,
+}
+
+#[derive(Debug, Clone)]
+pub struct StrategyResult {
+    pub status: StrategyStatus,
+    pub objective: Option<f64>,
+    pub best_bound: Option<f64>,
+    pub solution: Option<Vec<f64>>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum StrategyStatus {
+    Optimal,
+    Infeasible,
+    Stopped,
+    Error,
+}
+
+pub struct StrategySubprocess {
+    worker_id: WorkerId,
+    stop_flag: Arc<AtomicBool>,
+    last_progress: Arc<Mutex<Option<StrategyProgress>>>,
+}
+
+impl StrategySubprocess {
+    pub fn stop(&self) {
+        self.stop_flag.store(true, Ordering::Relaxed);
+    }
+
+    pub fn kill(&self, worker_manager: &WorkerManager) -> Result<(), String> {
+        worker_manager.kill_worker(self.worker_id)
+    }
+
+    pub fn last_progress(&self) -> Option<StrategyProgress> {
+        self.last_progress.lock().unwrap().clone()
+    }
+}
+
+fn send_via_stdin(stdin: &StdinWriter, msg: ResultMsg) {
+    let encoded = EncodedMsg::from(msg).encode();
+    let mut guard = stdin.lock().unwrap();
+    if let Some(writer) = guard.as_mut() {
+        let _ = writer.write_all(encoded.as_bytes());
+        let _ = writer.flush();
+    }
+}
+
+pub fn spawn_strategy(
+    worker_manager: &mut WorkerManager,
+    problem_desc: collomatique_ilp::ProblemDesc,
+    strategy: StrategyKind,
+    result_callback: impl Fn(StrategyResult) + Send + 'static,
+    progress_callback: impl Fn(&StrategyProgress) + Send + 'static,
+    log_callback: impl Fn(&str) + Send + 'static,
+) -> Result<StrategySubprocess, String> {
+    let request = StrategyRequest {
+        problem_desc,
+        strategy,
+    };
+    let serialized_str = request.serialize();
+    let serialized = SerializedStrategyRequest::from(serialized_str);
+    let init_msg = InitMsg::RunStrategy(serialized);
+
+    let stop_flag = Arc::new(AtomicBool::new(false));
+    let last_progress: Arc<Mutex<Option<StrategyProgress>>> = Arc::new(Mutex::new(None));
+    let stdin_slot: Arc<Mutex<Option<StdinWriter>>> = Arc::new(Mutex::new(None));
+
+    let stop_flag_cb = stop_flag.clone();
+    let last_progress_cb = last_progress.clone();
+    let stdin_slot_cb = stdin_slot.clone();
+
+    let callback = move |event: WorkerEvent| match event {
+        WorkerEvent::RpcCommand(Ok(cmd)) => match cmd {
+            collomatique_rpc::CmdMsg::Strategy(StrategyMsg::Progress(data)) => {
+                let progress = StrategyProgress {
+                    message: data.message,
+                    best_objective: data.best_objective.map(|v| v.into_inner()),
+                    feasible: data.feasible,
+                };
+                progress_callback(&progress);
+                *last_progress_cb.lock().unwrap() = Some(progress);
+
+                let stopped = stop_flag_cb.load(Ordering::Relaxed);
+                let response = ResultMsg::StrategyControl(!stopped);
+
+                let guard = stdin_slot_cb.lock().unwrap();
+                if let Some(stdin) = guard.as_ref() {
+                    send_via_stdin(stdin, response);
+                }
+            }
+            collomatique_rpc::CmdMsg::Strategy(StrategyMsg::Result(data)) => {
+                let result = StrategyResult {
+                    status: match data.status {
+                        collomatique_rpc::StrategyStatus::Optimal => StrategyStatus::Optimal,
+                        collomatique_rpc::StrategyStatus::Infeasible => StrategyStatus::Infeasible,
+                        collomatique_rpc::StrategyStatus::Stopped => StrategyStatus::Stopped,
+                        collomatique_rpc::StrategyStatus::Error => StrategyStatus::Error,
+                    },
+                    objective: data.objective.map(|v| v.into_inner()),
+                    best_bound: data.best_bound.map(|v| v.into_inner()),
+                    solution: data
+                        .solution
+                        .map(|s| s.into_iter().map(|v| v.into_inner()).collect()),
+                };
+
+                let guard = stdin_slot_cb.lock().unwrap();
+                if let Some(stdin) = guard.as_ref() {
+                    send_via_stdin(stdin, ResultMsg::Ack(None));
+                }
+                drop(guard);
+
+                result_callback(result);
+            }
+            _ => {}
+        },
+        WorkerEvent::LogLine(line) => {
+            log_callback(&line);
+        }
+        _ => {}
+    };
+
+    let worker_id = worker_manager.spawn_worker(init_msg, callback)?;
+
+    let stdin_writer = worker_manager.get_worker_stdin(worker_id);
+    *stdin_slot.lock().unwrap() = stdin_writer;
+
+    Ok(StrategySubprocess {
+        worker_id,
+        stop_flag,
+        last_progress,
+    })
+}
